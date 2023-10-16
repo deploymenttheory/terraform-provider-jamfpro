ddc3d0c0fb6f5e30ca6c97727caca168c57a2a6d
ShocOne <shocone@deploymenttheory.com>
2023-10-16 16:52:52 +0100

    schema tweaks

--- internal/provider/departments_resource.go
+++ internal/provider/departments_resource.go
@@ -28,7 +28,6 @@ func resourceJamfProDepartments() *schema.Resource {
 			"name": {
 				Type:        schema.TypeString,
 				Required:    true,
-				Optional:    true,
 				ForceNew:    true,
 				Description: "The unique name of the Jamf Pro department.",
 			},
