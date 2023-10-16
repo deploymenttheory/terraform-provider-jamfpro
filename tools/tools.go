399536235cfab95e04378af3d82ae71234e79319
ShocOne <shocone@deploymenttheory.com>
2023-10-16 11:29:24 +0100

    added new tools for code mgmt

--- tools/tools.go
+++ tools/tools.go
@@ -1,11 +1,13 @@
-// Copyright (c) HashiCorp, Inc.
-// SPDX-License-Identifier: MPL-2.0
-
 //go:build tools
+// +build tools
 
 package tools
 
 import (
-	// Documentation generation
+	_ "github.com/bflad/tfproviderlint/cmd/tfproviderlintx"
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs"
+	_ "github.com/katbyte/terrafmt"
+	_ "github.com/pavius/impi/cmd/impi"
+	_ "github.com/terraform-linters/tflint"
 )
