3aa3d223a3ee03270d28b6bd7107ea88a7dcd186
ShocOne <shocone@deploymenttheory.com>
2023-10-24 15:28:54 +0100

    tweaks to jitter backoff time

--- internal/resources/computerextensionattributes/computerextensionattributes_resource.go
+++ internal/resources/computerextensionattributes/computerextensionattributes_resource.go
@@ -22,12 +22,17 @@ const (
 	maxDelay   = 30 * time.Second // Max delay of 30 seconds
 )
 
+// exponentialBackoffWithJitter computes the wait duration for the current Resource Read retry attempt.
+// The function uses exponential backoff with jitter to introduce randomness. This helps distribute the
+// request load evenly and reduces the chances of "thundering herd" issues. The formula starts with a
+// delay of 3 seconds (3<<attempt) and introduces a random jitter.
 func exponentialBackoffWithJitter(attempt int) time.Duration {
-	expBackoff := time.Duration(1<<attempt) * time.Second
+	expBackoff := time.Duration(3<<attempt) * time.Second
 	jitter := time.Duration(rand.Int63n(int64(expBackoff)))
 	return time.Duration(minDuration(expBackoff+jitter, maxDelay))
 }
 
+// minDuration returns the smaller of the two provided time durations.
 func minDuration(a, b time.Duration) time.Duration {
 	if a < b {
 		return a
