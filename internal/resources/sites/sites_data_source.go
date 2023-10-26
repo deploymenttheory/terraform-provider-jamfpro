844a05e8cf3aa2938f5c1901c011863e35d5d7c0
ShocOne <shocone@deploymenttheory.com>
2023-10-26 12:43:48 +0100

    added support for jamf pro sites

---
+++ internal/resources/sites/sites_data_source.go
@@ -0,0 +1,80 @@
+// sites_date_source.go
+package sites
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+
+	"github.com/deploymenttheory/go-api-sdk-jamfpro/sdk/jamfpro"
+	"github.com/deploymenttheory/terraform-provider-jamfpro/internal/client"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+// DataSourceJamfProSites provides information about a specific Jamf Pro site by its ID or Name.
+func DataSourceJamfProSites() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceJamfProSitesRead,
+		Schema: map[string]*schema.Schema{
+			"id": {
+				Type:        schema.TypeInt,
+				Optional:    true,
+				Description: "The unique identifier of the Jamf Pro site.",
+				Computed:    true,
+			},
+			"name": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "The unique name of the Jamf Pro site.",
+				Computed:    true,
+			},
+		},
+	}
+}
+
+// dataSourceJamfProSitesRead fetches the details of a specific Jamf Pro site
+// from Jamf Pro using either its unique Name or its Id. The function prioritizes the 'name' attribute over the 'id'
+// attribute for fetching details. If neither 'name' nor 'id' is provided, it returns an error.
+// Once the details are fetched, they are set in the data source's state.
+//
+// Parameters:
+// - ctx: The context within which the function is called. It's used for timeouts and cancellation.
+// - d: The current state of the data source.
+// - meta: The meta object that can be used to retrieve the API client connection.
+//
+// Returns:
+// - diag.Diagnostics: Returns any diagnostics (errors or warnings) encountered during the function's execution.
+func dataSourceJamfProSitesRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conn := meta.(*client.APIClient).Conn
+
+	var site *jamfpro.SiteResponse
+	var err error
+
+	// Check if Name is provided in the data source configuration
+	if v, ok := d.GetOk("name"); ok && v.(string) != "" {
+		siteName := v.(string)
+		site, err = conn.GetSiteByName(siteName)
+		if err != nil {
+			return diag.FromErr(fmt.Errorf("failed to fetch Jamf Pro site by name: %v", err))
+		}
+	} else if v, ok := d.GetOk("id"); ok {
+		siteID, convertErr := strconv.Atoi(v.(string))
+		if convertErr != nil {
+			return diag.FromErr(fmt.Errorf("failed to convert Jamf Pro site ID to integer: %v", convertErr))
+		}
+		site, err = conn.GetSiteByID(siteID)
+		if err != nil {
+			return diag.FromErr(fmt.Errorf("failed to fetch Jamf Pro site by ID: %v", err))
+		}
+	} else {
+		return diag.Errorf("Either 'name' or 'id' must be provided")
+	}
+
+	// Set the data source attributes using the fetched data
+	d.SetId(fmt.Sprintf("%d", site.ID))
+	d.Set("name", site.Name)
+
+	return nil
+}
---
+++ internal/resources/sites/sites_resource.go
@@ -0,0 +1,298 @@
+// sites_resource.go
+package sites
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/deploymenttheory/go-api-sdk-jamfpro/sdk/http_client"
+	"github.com/deploymenttheory/go-api-sdk-jamfpro/sdk/jamfpro"
+	"github.com/deploymenttheory/terraform-provider-jamfpro/internal/client"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+// ResourceJamfProSite defines the schema and CRUD operations for managing Jamf Pro Sites in Terraform.
+func ResourceJamfProSite() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: ResourceJamfProSitesCreate,
+		ReadContext:   ResourceJamfProSitesRead,
+		UpdateContext: ResourceJamfProSitesUpdate,
+		DeleteContext: ResourceJamfProSitesDelete,
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(30 * time.Minute),
+			Read:   schema.DefaultTimeout(10 * time.Minute),
+			Update: schema.DefaultTimeout(30 * time.Minute),
+			Delete: schema.DefaultTimeout(15 * time.Minute),
+		},
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+		Schema: map[string]*schema.Schema{
+			"id": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The unique identifier of the site.",
+			},
+			"name": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "The unique name of the Jamf Pro site.",
+			},
+		},
+	}
+}
+
+// constructSite constructs a SiteResponse object from the provided schema data.
+// It captures attributes from the schema and returns the constructed object.
+func constructSite(d *schema.ResourceData) *jamfpro.SiteResponse {
+	return &jamfpro.SiteResponse{
+		Name: d.Get("name").(string),
+	}
+}
+
+// Helper function to generate diagnostics based on the error type
+func generateTFDiagsFromHTTPError(err error, d *schema.ResourceData, action string) diag.Diagnostics {
+	var diags diag.Diagnostics
+	resourceName, exists := d.GetOk("name")
+	if !exists {
+		resourceName = "unknown"
+	}
+
+	// Handle the APIError in the diagnostic
+	if apiErr, ok := err.(*http_client.APIError); ok {
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  fmt.Sprintf("Failed to %s the resource with name: %s", action, resourceName),
+			Detail:   fmt.Sprintf("API Error (Code: %d): %s", apiErr.StatusCode, apiErr.Message),
+		})
+	} else {
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  fmt.Sprintf("Failed to %s the resource with name: %s", action, resourceName),
+			Detail:   err.Error(),
+		})
+	}
+	return diags
+}
+
+// ResourceJamfProSitesCreate is responsible for creating a new Jamf Pro Site in the remote system.
+// The function:
+// 1. Constructs the attribute data using the provided Terraform configuration.
+// 2. Calls the API to create the attribute in Jamf Pro.
+// 3. Updates the Terraform state with the ID of the newly created attribute.
+// 4. Initiates a read operation to synchronize the Terraform state with the actual state in Jamf Pro.
+func ResourceJamfProSitesCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conn := meta.(*client.APIClient).Conn
+	var diags diag.Diagnostics
+
+	// Use the retry function for the create operation
+	var createdAttribute *jamfpro.SiteResponse
+	var err error
+	err = retry.RetryContext(ctx, d.Timeout(schema.TimeoutCreate), func() *retry.RetryError {
+		// Construct the computer extension attribute
+		attribute := constructSite(d)
+
+		// Check if the attribute is nil (indicating an issue with input_type)
+		if attribute == nil {
+			return retry.NonRetryableError(fmt.Errorf("failed to construct the computer extension attribute due to missing or invalid input_type"))
+		}
+
+		// Directly call the API to create the resource
+		createdAttribute, err = conn.CreateSite(attribute)
+		if err != nil {
+			// Check if the error is an APIError
+			if apiErr, ok := err.(*http_client.APIError); ok {
+				return retry.NonRetryableError(fmt.Errorf("API Error (Code: %d): %s", apiErr.StatusCode, apiErr.Message))
+			}
+			// For simplicity, we're considering all other errors as retryable
+			return retry.RetryableError(err)
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		// If there's an error while creating the resource, generate diagnostics using the helper function.
+		return generateTFDiagsFromHTTPError(err, d, "create")
+	}
+
+	// Set the ID of the created resource in the Terraform state
+	d.SetId(strconv.Itoa(createdAttribute.ID))
+
+	// Use the retry function for the read operation to update the Terraform state with the resource attributes
+	err = retry.RetryContext(ctx, d.Timeout(schema.TimeoutRead), func() *retry.RetryError {
+		readDiags := ResourceJamfProSitesRead(ctx, d, meta)
+		if len(readDiags) > 0 {
+			// If readDiags is not empty, it means there's an error, so we retry
+			return retry.RetryableError(fmt.Errorf("failed to read the created resource"))
+		}
+		return nil
+	})
+
+	if err != nil {
+		// If there's an error while updating the state for the resource, generate diagnostics using the helper function.
+		return generateTFDiagsFromHTTPError(err, d, "update state for")
+	}
+
+	return diags
+}
+
+// ResourceJamfProSitesRead is responsible for reading the current state of a Jamf Pro Site Resource from the remote system.
+// The function:
+// 1. Fetches the attribute's current state using its ID. If it fails then obtain attribute's current state using its Name.
+// 2. Updates the Terraform state with the fetched data to ensure it accurately reflects the current state in Jamf Pro.
+// 3. Handles any discrepancies, such as the attribute being deleted outside of Terraform, to keep the Terraform state synchronized.
+func ResourceJamfProSitesRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conn := meta.(*client.APIClient).Conn
+	var diags diag.Diagnostics
+
+	var attribute *jamfpro.SiteResponse
+
+	// Use the retry function for the read operation
+	err := retry.RetryContext(ctx, d.Timeout(schema.TimeoutRead), func() *retry.RetryError {
+		// Convert the ID from the Terraform state into an integer to be used for the API request
+		attributeID, convertErr := strconv.Atoi(d.Id())
+		if convertErr != nil {
+			return retry.NonRetryableError(fmt.Errorf("failed to parse attribute ID: %v", convertErr))
+		}
+
+		// Try fetching the site using the ID
+		var apiErr error
+		attribute, apiErr = conn.GetSiteByID(attributeID)
+		if apiErr != nil {
+			// Handle the APIError
+			if apiError, ok := apiErr.(*http_client.APIError); ok {
+				return retry.NonRetryableError(fmt.Errorf("API Error (Code: %d): %s", apiError.StatusCode, apiError.Message))
+			}
+			// If fetching by ID fails, try fetching by Name
+			attributeName := d.Get("name").(string)
+			attribute, apiErr = conn.GetSiteByName(attributeName)
+			if apiErr != nil {
+				// Handle the APIError
+				if apiError, ok := apiErr.(*http_client.APIError); ok {
+					return retry.NonRetryableError(fmt.Errorf("API Error (Code: %d): %s", apiError.StatusCode, apiError.Message))
+				}
+				return retry.RetryableError(apiErr)
+			}
+		}
+		return nil
+	})
+
+	// Handle error from the retry function
+	if err != nil {
+		// If there's an error while reading the resource, generate diagnostics using the helper function.
+		return generateTFDiagsFromHTTPError(err, d, "read")
+	}
+
+	// Safely set attributes in the Terraform state
+	if err := d.Set("name", attribute.Name); err != nil {
+		diags = append(diags, diag.FromErr(err)...)
+	}
+
+	return diags
+}
+
+// ResourceJamfProSitesUpdate is responsible for updating an existing Jamf Pro Site on the remote system.
+func ResourceJamfProSitesUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conn := meta.(*client.APIClient).Conn
+	var diags diag.Diagnostics
+
+	// Use the retry function for the update operation
+	var err error
+	err = retry.RetryContext(ctx, d.Timeout(schema.TimeoutUpdate), func() *retry.RetryError {
+		// Construct the updated site
+		site := constructSite(d)
+
+		// Convert the ID from the Terraform state into an integer to be used for the API request
+		siteID, convertErr := strconv.Atoi(d.Id())
+		if convertErr != nil {
+			return retry.NonRetryableError(fmt.Errorf("failed to parse site ID: %v", convertErr))
+		}
+
+		// Directly call the API to update the resource
+		_, apiErr := conn.UpdateSiteByID(siteID, site)
+		if apiErr != nil {
+			// Handle the APIError
+			if apiError, ok := apiErr.(*http_client.APIError); ok {
+				return retry.NonRetryableError(fmt.Errorf("API Error (Code: %d): %s", apiError.StatusCode, apiError.Message))
+			}
+			// If the update by ID fails, try updating by name
+			siteName := d.Get("name").(string)
+			_, apiErr = conn.UpdateSiteByName(siteName, site)
+			if apiErr != nil {
+				// Handle the APIError
+				if apiError, ok := apiErr.(*http_client.APIError); ok {
+					return retry.NonRetryableError(fmt.Errorf("API Error (Code: %d): %s", apiError.StatusCode, apiError.Message))
+				}
+				return retry.RetryableError(apiErr)
+			}
+		}
+		return nil
+	})
+
+	// Handle error from the retry function
+	if err != nil {
+		// If there's an error while updating the resource, generate diagnostics using the helper function.
+		return generateTFDiagsFromHTTPError(err, d, "update")
+	}
+
+	// Use the retry function for the read operation to update the Terraform state
+	err = retry.RetryContext(ctx, d.Timeout(schema.TimeoutRead), func() *retry.RetryError {
+		readDiags := ResourceJamfProSitesRead(ctx, d, meta)
+		if len(readDiags) > 0 {
+			return retry.RetryableError(fmt.Errorf("failed to update the Terraform state for the updated resource"))
+		}
+		return nil
+	})
+
+	// Handle error from the retry function
+	if err != nil {
+		// If there's an error while updating the resource, generate diagnostics using the helper function.
+		return generateTFDiagsFromHTTPError(err, d, "update")
+	}
+
+	return diags
+}
+
+// ResourceJamfProSitesDelete is responsible for deleting a Jamf Pro Site.
+func ResourceJamfProSitesDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conn := meta.(*client.APIClient).Conn
+	var diags diag.Diagnostics
+
+	// Use the retry function for the delete operation
+	err := retry.RetryContext(ctx, d.Timeout(schema.TimeoutDelete), func() *retry.RetryError {
+		// Convert the ID from the Terraform state into an integer to be used for the API request
+		siteID, convertErr := strconv.Atoi(d.Id())
+		if convertErr != nil {
+			return retry.NonRetryableError(fmt.Errorf("failed to parse site ID: %v", convertErr))
+		}
+
+		// Directly call the API to delete the resource
+		apiErr := conn.DeleteSiteByID(siteID)
+		if apiErr != nil {
+			// If the delete by ID fails, try deleting by name
+			siteName := d.Get("name").(string)
+			apiErr = conn.DeleteSiteByName(siteName)
+			if apiErr != nil {
+				return retry.RetryableError(apiErr)
+			}
+		}
+		return nil
+	})
+
+	// Handle error from the retry function
+	if err != nil {
+		// If there's an error while deleting the resource, generate diagnostics using the helper function.
+		return generateTFDiagsFromHTTPError(err, d, "delete")
+	}
+
+	// Clear the ID from the Terraform state as the resource has been deleted
+	d.SetId("")
+
+	return diags
+}
