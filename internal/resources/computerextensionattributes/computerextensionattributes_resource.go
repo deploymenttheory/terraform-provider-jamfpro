46dc3f0238d510d0e49cf1184b5acf8d4a10c680
ShocOne <shocone@deploymenttheory.com>
2023-10-27 13:23:09 +0100

    func rearrangement

--- internal/resources/computerextensionattributes/computerextensionattributes_resource.go
+++ internal/resources/computerextensionattributes/computerextensionattributes_resource.go
@@ -4,7 +4,6 @@ package computerextensionattributes
 import (
 	"context"
 	"fmt"
-	"regexp"
 	"strconv"
 	"time"
 
@@ -17,20 +16,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 )
-
-// validateDataType ensures the provided value adheres to the accepted formats for the data_type attribute.
-// The accepted formats are "String", "Integer", and a date string in the "YYYY-MM-DD hh:mm:ss" format.
-func validateDataType(val interface{}, key string) (warns []string, errs []error) {
-	value := val.(string)
-
-	// Regular expression to validate the date format "YYYY-MM-DD hh:mm:ss"
-	datePattern := `^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$`
-
-	if value != "String" && value != "Integer" && !regexp.MustCompile(datePattern).MatchString(value) {
-		errs = append(errs, fmt.Errorf("%q must be 'String', 'Integer', or 'YYYY-MM-DD hh:mm:ss' format, got: %s", key, value))
-	}
-	return
-}
 
 // ResourceJamfProComputerExtensionAttributes defines the schema and CRUD operations (Create, Read, Update, Delete)
 // for managing Jamf Pro Computer Extension Attributes in Terraform.
