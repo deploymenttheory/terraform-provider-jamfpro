db559bc2f04c8170e5b811465d26d55b7292e45d
ShocOne <shocone@deploymenttheory.com>
2023-10-27 13:58:47 +0100

    imrpoved logic

--- internal/importer/departments_resource_importer.go
+++ internal/importer/departments_resource_importer.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 
 	"github.com/deploymenttheory/go-api-sdk-jamfpro/sdk/jamfpro"
 )
@@ -25,7 +26,7 @@ func main() {
 
 func initializeJamfProClient() *jamfpro.Client {
 	// Load client authentication configuration from a JSON file
-	authConfig, err := jamfpro.LoadClientAuthConfig("/Users/dafyddwatkins/GitHub/deploymenttheory/terraform-provider-jamfpro/clientauth.json")
+	authConfig, err := jamfpro.LoadClientAuthConfig("/Users/dafyddwatkins/GitHub/deploymenttheory/terraform-provider-jamfpro/.localtesting/clientauth.json")
 	if err != nil {
 		log.Fatalf("Failed to load client authentication configuration: %v", err)
 	}
@@ -51,11 +52,26 @@ func initializeJamfProClient() *jamfpro.Client {
 func generateTerraformConfig(client *jamfpro.Client, departmentIDs *jamfpro.ResponseDepartmentsList) {
 	log.Println("Starting generation of Terraform config...")
 
-	// Open a file for writing the Terraform configuration
-	f, err := os.Create("departments.tf")
-	if err != nil {
-		log.Fatalf("Failed to create Terraform file: %v", err)
+	// Check if departments.tf already exists
+	filePath := "departments.tf"
+	var f *os.File
+
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		// Create the file if it doesn't exist
+		var err error
+		f, err = os.Create(filePath)
+		if err != nil {
+			log.Fatalf("Failed to create Terraform file: %v", err)
+		}
+	} else {
+		// Open the file in append mode if it exists
+		var err error
+		f, err = os.OpenFile(filePath, os.O_APPEND|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Fatalf("Failed to open Terraform file: %v", err)
+		}
 	}
+
 	defer func() {
 		if err := f.Close(); err != nil {
 			log.Fatalf("Failed to close Terraform file: %v", err)
@@ -97,17 +113,50 @@ func generateDepartmentHCL(department jamfpro.ResponseDepartment) string {
 resource "jamfpro_department" "department_%d" {
   name = "%s"
 }
-`, department.ID, department.Name) // Note: Changed from department.Id to department.ID
+`, department.ID, department.Name)
 
 	return hcl
 }
 
+// isResourceImported checks if a given resource is already imported in the Terraform state.
+// It returns true if the resource is imported, false otherwise.
+func isResourceImported(resourceAddress string) (bool, error) {
+	cmd := exec.Command("terraform", "state", "list", resourceAddress)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return false, fmt.Errorf("failed to execute terraform state list: %v, output: %s", err, output)
+	}
+
+	// If the output contains the resource address, it means the resource is already imported.
+	if strings.Contains(string(output), resourceAddress) {
+		return true, nil
+	}
+
+	return false, nil
+}
+
+// importIntoTerraformState imports the department into the Terraform state if not already imported.
 func importIntoTerraformState(departmentID int) error {
-	resourceAddress := fmt.Sprintf("jamfpro_departments.department_%d", departmentID)
+	resourceAddress := fmt.Sprintf("jamfpro_department.department_%d", departmentID)
+
+	// Check if the resource is already imported
+	imported, err := isResourceImported(resourceAddress)
+	if err != nil {
+		return fmt.Errorf("failed to check if resource is imported: %v", err)
+	}
+
+	if imported {
+		log.Printf("Resource %s is already imported into Terraform state.", resourceAddress)
+		return nil
+	}
+
+	// Import the resource
 	cmd := exec.Command("terraform", "import", resourceAddress, fmt.Sprint(departmentID))
 	output, err := cmd.CombinedOutput()
 	if err != nil {
 		return fmt.Errorf("failed to run terraform import: %v, output: %s", err, output)
 	}
+
+	log.Printf("Successfully imported resource %s into Terraform state.", resourceAddress)
 	return nil
 }
