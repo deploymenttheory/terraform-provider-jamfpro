7c7f50762bf96d4e5b56f6e9aa11d004cb7fefc4
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-02-06 13:50:10 +0000

    Update disk encryption configuration data source

--- internal/endpoints/diskencryptionconfigurations/diskencryptionconfigurations_data_source.go
+++ internal/endpoints/diskencryptionconfigurations/diskencryptionconfigurations_data_source.go
@@ -25,7 +25,7 @@ func DataSourceJamfProDiskEncryptionConfigurations() *schema.Resource {
 		Schema: map[string]*schema.Schema{
 			"id": {
 				Type:        schema.TypeString,
-				Computed:    true,
+				Required:    true,
 				Description: "The unique identifier of the disk encryption configuration.",
 			},
 			"name": {
@@ -33,49 +33,12 @@ func DataSourceJamfProDiskEncryptionConfigurations() *schema.Resource {
 				Computed:    true,
 				Description: "The name of the disk encryption configuration.",
 			},
-			"key_type": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "The type of the key used in the disk encryption which can be either 'Institutional' or 'Individual and Institutional'.",
-			},
-			"file_vault_enabled_users": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "Defines which user to enable for FileVault 2. Value can be either 'Management Account' or 'Current or Next User'",
-			},
-			"institutional_recovery_key": {
-				Type:        schema.TypeList,
-				Computed:    true,
-				Description: "Details of the institutional recovery key.",
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"key": {
-							Type:     schema.TypeString,
-							Computed: true,
-						},
-						"certificate_type": {
-							Type:        schema.TypeString,
-							Description: "The type of certificate used for the institutional recovery key. e.g 'PKCS12' for .p12 certificate types.",
-							Computed:    true,
-						},
-						"password": {
-							Type:        schema.TypeString,
-							Description: "The password for the institutional recovery key certificate.",
-							Computed:    true,
-							Sensitive:   true,
-						},
-						"data": {
-							Type:        schema.TypeString,
-							Description: "The certificate payload.",
-							Computed:    true,
-						},
-					},
-				},
-			},
 		},
 	}
 }
 
+// DataSourceJamfProDiskEncryptionConfigurationsRead fetches the details of a specific Jamf Pro disk encryption configuration
+// from Jamf Pro and returns the details of the disk encryption configuration in the Terraform state.
 func DataSourceJamfProDiskEncryptionConfigurationsRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	// Initialize api client
 	apiclient, ok := meta.(*client.APIClient)
@@ -89,9 +52,11 @@ func DataSourceJamfProDiskEncryptionConfigurationsRead(ctx context.Context, d *s
 
 	// Initialize variables
 	var diags diag.Diagnostics
-	resourceID := d.Id()
 	var apiErrorCode int
-	var diskEncryptionConfig *jamfpro.ResourceDiskEncryptionConfiguration
+	var script *jamfpro.ResourceDiskEncryptionConfiguration
+
+	// Get the distribution point ID from the data source's arguments
+	resourceID := d.Get("id").(string)
 
 	// Convert resourceID from string to int
 	resourceIDInt, err := strconv.Atoi(resourceID)
@@ -100,47 +65,32 @@ func DataSourceJamfProDiskEncryptionConfigurationsRead(ctx context.Context, d *s
 		logging.LogTypeConversionFailure(subCtx, "string", "int", JamfProResourceDiskEncryptionConfiguration, resourceID, err.Error())
 		return diag.FromErr(err)
 	}
-
 	// Read operation with retry
 	err = retry.RetryContext(subCtx, d.Timeout(schema.TimeoutRead), func() *retry.RetryError {
 		var apiErr error
-		diskEncryptionConfig, apiErr = conn.GetDiskEncryptionConfigurationByID(resourceIDInt)
+		script, apiErr = conn.GetDiskEncryptionConfigurationByID(resourceIDInt)
 		if apiErr != nil {
 			logging.LogFailedReadByID(subCtx, JamfProResourceDiskEncryptionConfiguration, resourceID, apiErr.Error(), apiErrorCode)
 			// Convert any API error into a retryable error to continue retrying
 			return retry.RetryableError(apiErr)
 		}
-		// Successfully read the account group, exit the retry loop
+		// Successfully read the data, exit the retry loop
 		return nil
 	})
 
 	if err != nil {
+		// Handle the final error after all retries have been exhausted
 		return diag.FromErr(err)
 	}
 
-	// Assuming successful retrieval, proceed to set the resource attributes in Terraform state
-	d.SetId(strconv.Itoa(resourceIDInt)) // Update the ID in the state
-	d.Set("name", diskEncryptionConfig.Name)
-	d.Set("key_type", diskEncryptionConfig.KeyType)
-	d.Set("file_vault_enabled_users", diskEncryptionConfig.FileVaultEnabledUsers)
-
-	// Institutional Recovery Key
-	if diskEncryptionConfig.InstitutionalRecoveryKey == nil ||
-		(diskEncryptionConfig.InstitutionalRecoveryKey.Key == "" &&
-			diskEncryptionConfig.InstitutionalRecoveryKey.CertificateType == "" &&
-			diskEncryptionConfig.InstitutionalRecoveryKey.Password == "" &&
-			diskEncryptionConfig.InstitutionalRecoveryKey.Data == "") {
-
-		// If InstitutionalRecoveryKey is nil or empty, ensure it is not set in the Terraform state
-		d.Set("institutional_recovery_key", []interface{}{})
+	// Check if resource data exists and set the Terraform state
+	if script != nil {
+		d.SetId(resourceID) // Set the id in the Terraform state
+		if err := d.Set("name", script.Name); err != nil {
+			diags = append(diags, diag.FromErr(err)...)
+		}
 	} else {
-		// If InstitutionalRecoveryKey has data, set it in the Terraform state
-		irk := make(map[string]interface{})
-		irk["certificate_type"] = diskEncryptionConfig.InstitutionalRecoveryKey.CertificateType
-		irk["password"] = diskEncryptionConfig.InstitutionalRecoveryKey.Password
-		irk["data"] = diskEncryptionConfig.InstitutionalRecoveryKey.Data
-
-		d.Set("institutional_recovery_key", []interface{}{irk})
+		d.SetId("") // Data not found, unset the id in the Terraform state
 	}
 
 	return diags
