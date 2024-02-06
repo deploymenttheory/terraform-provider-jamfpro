ae4c442e660c50676ed907e1b4564eb4bf641d98
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-02-06 13:11:38 +0000

    Refactor dock item output names

--- examples/dockitems/datasource.tf
+++ examples/dockitems/datasource.tf
@@ -1,23 +1,11 @@
 data "jamfpro_dock_item" "example_dock_item" {
-  id = "12345"  # Replace this with the actual ID of the dock item you want to retrieve
+  id = resource.jamfpro_dock_item.example_dock_item.id
 }
 
-output "dock_item_id" {
+output "jamfpro_dock_item_id" {
   value = data.jamfpro_dock_item.example_dock_item.id
 }
 
-output "dock_item_name" {
+output "jamfpro_dock_item_name" {
   value = data.jamfpro_dock_item.example_dock_item.name
-}
-
-output "dock_item_type" {
-  value = data.jamfpro_dock_item.example_dock_item.type
-}
-
-output "dock_item_path" {
-  value = data.jamfpro_dock_item.example_dock_item.path
-}
-
-output "dock_item_contents" {
-  value = data.jamfpro_dock_item.example_dock_item.contents
 }
