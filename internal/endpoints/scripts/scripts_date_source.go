e6ca3ff9d1fd547796b2a628ede21503d16bd932
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-02-06 13:21:59 +0000

    Refactor Jamf Pro script data source function

--- internal/endpoints/scripts/scripts_date_source.go
+++ internal/endpoints/scripts/scripts_date_source.go
@@ -29,97 +29,12 @@ func DataSourceJamfProScripts() *schema.Resource {
 				Required:    true,
 				Description: "Display name for the script.",
 			},
-			"category_name": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "Name of the category to add the script to.",
-			},
-			"category_id": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "The Jamf Pro unique identifier (ID) of the category.",
-			},
-			"info": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "Information to display to the administrator when the script is run.",
-			},
-			"notes": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "Notes to display about the script (e.g., who created it and when it was created).",
-			},
-			"os_requirements": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "The script can only be run on computers with these operating system versions. Each version must be separated by a comma (e.g., 10.11, 15, 16.1).",
-			},
-			"priority": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "Execution priority of the script (Before, After, At Reboot).",
-			},
-			"script_contents": {
-				Type:        schema.TypeString,
-				Required:    true,
-				Description: "Contents of the script. Must be non-compiled and in an accepted format.",
-			},
-			"parameter4": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "Script parameter label 4",
-			},
-			"parameter5": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "Script parameter label 5",
-			},
-			"parameter6": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "Script parameter label 6",
-			},
-			"parameter7": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "Script parameter label 7",
-			},
-			"parameter8": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "Script parameter label 8",
-			},
-			"parameter9": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "Script parameter label 9",
-			},
-			"parameter10": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "Script parameter label 10",
-			},
-			"parameter11": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "Script parameter label 11",
-			},
 		},
 	}
 }
 
 // dataSourceJamfProScriptsRead fetches the details of a specific Jamf Pro script
-// from Jamf Pro using either its unique Name or its Id. The function prioritizes the 'name' scriptAttribute over the 'id'
-// scriptAttribute for fetching details. If neither 'name' nor 'id' is provided, it returns an error.
-// Once the details are fetched, they are set in the data source's state.
-//
-// Parameters:
-// - ctx: The context within which the function is called. It's used for timeouts and cancellation.
-// - d: The current state of the data source.
-// - meta: The meta object that can be used to retrieve the API client connection.
-//
-// Returns:
-// - diag.Diagnostics: Returns any diagnostics (errors or warnings) encountered during the function's execution.
+// from Jamf Pro using either its unique Name or its Id.
 func DataSourceJamfProScriptsRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	// Initialize api client
 	apiclient, ok := meta.(*client.APIClient)
@@ -135,7 +50,9 @@ func DataSourceJamfProScriptsRead(ctx context.Context, d *schema.ResourceData, m
 	var diags diag.Diagnostics
 	var apiErrorCode int
 	var script *jamfpro.ResourceScript
-	resourceID := d.Id()
+
+	// Get the distribution point ID from the data source's arguments
+	resourceID := d.Get("id").(string)
 
 	// Read operation with retry
 	err := retry.RetryContext(subCtx, d.Timeout(schema.TimeoutRead), func() *retry.RetryError {
@@ -146,40 +63,23 @@ func DataSourceJamfProScriptsRead(ctx context.Context, d *schema.ResourceData, m
 			// Convert any API error into a retryable error to continue retrying
 			return retry.RetryableError(apiErr)
 		}
-		// Successfully read the script, exit the retry loop
+		// Successfully read the data, exit the retry loop
 		return nil
 	})
 
 	if err != nil {
+		// Handle the final error after all retries have been exhausted
 		return diag.FromErr(err)
 	}
 
-	// Construct a map of script attributes
-	scriptAttributes := map[string]interface{}{
-		"name":            script.Name,
-		"category_name":   script.CategoryName,
-		"category_id":     script.CategoryId,
-		"info":            script.Info,
-		"notes":           script.Notes,
-		"os_requirements": script.OSRequirements,
-		"priority":        script.Priority,
-		"script_contents": encodeScriptContent(script.ScriptContents),
-		"parameter4":      script.Parameter4,
-		"parameter5":      script.Parameter5,
-		"parameter6":      script.Parameter6,
-		"parameter7":      script.Parameter7,
-		"parameter8":      script.Parameter8,
-		"parameter9":      script.Parameter9,
-		"parameter10":     script.Parameter10,
-		"parameter11":     script.Parameter11,
-	}
-
-	// Update the Terraform state with script scripts
-	for key, value := range scriptAttributes {
-		if err := d.Set(key, value); err != nil {
+	// Check if resource data exists and set the Terraform state
+	if script != nil {
+		d.SetId(resourceID) // Set the id in the Terraform state
+		if err := d.Set("name", script.Name); err != nil {
 			diags = append(diags, diag.FromErr(err)...)
-			return diags
 		}
+	} else {
+		d.SetId("") // Data not found, unset the id in the Terraform state
 	}
 
 	return diags
