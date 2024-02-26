ea66e8fe152e50e2458d0e96e36475f6e6379596
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-02-26 06:12:22 +0000

    Update resource and data source names

--- examples/sites/datasource.tf
+++ examples/sites/datasource.tf
@@ -1,23 +1,23 @@
-data "jamfpro_sites" "site_001_data" {
-  id = jamfpro_sites.site_001.id
+data "jamfpro_site" "site_001_data" {
+  id = jamfpro_site.site_001.id
 }
 
 output "jamfpro_site_001_id" {
-  value = data.jamfpro_sites.site_001_data.id
+  value = data.jamfpro_site.site_001_data.id
 }
 
 output "jamfpro_site_001_name" {
-  value = data.jamfpro_sites.site_001_data.name
+  value = data.jamfpro_site.site_001_data.name
 }
 
 data "jamfpro_sites" "site_002_data" {
-  id = jamfpro_sites.site_002.id
+  id = jamfpro_site.site_002.id
 }
 
 output "jamfpro_site_002_id" {
-  value = data.jamfpro_sites.site_002_data.id
+  value = data.jamfpro_site.site_002_data.id
 }
 
 output "jamfpro_site_002_name" {
-  value = data.jamfpro_sites.site_002_data.name
+  value = data.jamfpro_site.site_002_data.name
 }
