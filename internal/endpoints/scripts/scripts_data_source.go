149ea3bc661c857d941b23354b2b876d1890225e
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-02-26 07:43:49 +0000

    Refactor Jamf Pro script data source

--- internal/endpoints/scripts/scripts_data_source.go
+++ internal/endpoints/scripts/scripts_data_source.go
@@ -3,13 +3,12 @@ package scripts
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/deploymenttheory/go-api-sdk-jamfpro/sdk/jamfpro"
 	"github.com/deploymenttheory/terraform-provider-jamfpro/internal/client"
-	"github.com/deploymenttheory/terraform-provider-jamfpro/internal/logging"
 
-	"github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -40,50 +39,43 @@ func DataSourceJamfProScripts() *schema.Resource {
 // DataSourceJamfProScriptsRead fetches the details of a specific Jamf Pro script
 // from Jamf Pro using either its unique Name or its Id.
 func DataSourceJamfProScriptsRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	// Initialize api client
+	// Initialize API client
 	apiclient, ok := meta.(*client.APIClient)
 	if !ok {
 		return diag.Errorf("error asserting meta as *client.APIClient")
 	}
 	conn := apiclient.Conn
 
-	// Initialize the logging subsystem for the read operation
-	subCtx := logging.NewSubsystemLogger(ctx, logging.SubsystemRead, hclog.Info)
-
 	// Initialize variables
 	var diags diag.Diagnostics
-	var apiErrorCode int
+	resourceID := d.Get("id").(string)
 	var script *jamfpro.ResourceScript
 
-	// Get the distribution point ID from the data source's arguments
-	resourceID := d.Get("id").(string)
-
 	// Read operation with retry
-	err := retry.RetryContext(subCtx, d.Timeout(schema.TimeoutRead), func() *retry.RetryError {
+	err := retry.RetryContext(ctx, d.Timeout(schema.TimeoutRead), func() *retry.RetryError {
 		var apiErr error
 		script, apiErr = conn.GetScriptByID(resourceID)
 		if apiErr != nil {
-			logging.LogFailedReadByID(subCtx, JamfProResourceScript, resourceID, apiErr.Error(), apiErrorCode)
 			// Convert any API error into a retryable error to continue retrying
 			return retry.RetryableError(apiErr)
 		}
-		// Successfully read the data, exit the retry loop
+		// Successfully read the script, exit the retry loop
 		return nil
 	})
 
 	if err != nil {
 		// Handle the final error after all retries have been exhausted
-		return diag.FromErr(err)
+		return diag.FromErr(fmt.Errorf("failed to read Jamf Pro Script with ID '%s' after retries: %v", resourceID, err))
 	}
 
 	// Check if resource data exists and set the Terraform state
 	if script != nil {
-		d.SetId(resourceID) // Set the id in the Terraform state
+		d.SetId(resourceID) // Confirm the ID in the Terraform state
 		if err := d.Set("name", script.Name); err != nil {
-			diags = append(diags, diag.FromErr(err)...)
+			diags = append(diags, diag.FromErr(fmt.Errorf("error setting 'name' for Jamf Pro Script with ID '%s': %v", resourceID, err))...)
 		}
 	} else {
-		d.SetId("") // Data not found, unset the id in the Terraform state
+		d.SetId("") // Data not found, unset the ID in the Terraform state
 	}
 
 	return diags
