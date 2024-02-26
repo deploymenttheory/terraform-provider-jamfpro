40e9d6df27066051b534c3c53bc9db5c1ed64c94
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-02-26 06:10:00 +0000

    Update resource names and fix typos

--- examples/accountgroups/datasource.tf
+++ examples/accountgroups/datasource.tf
@@ -1,11 +1,11 @@
-data "jamfpro_account_groups" "jamfpro_account_groups_001_data" {
-  id = jamfpro_account_groups.jamfpro_account_groups_001.id
+data "jamfpro_account_group" "jamfpro_account_group_001_data" {
+  id = jamfpro_account_group.jamfpro_account_group_001.id
 }
 
-output "jamfpro_jamfpro_account_groups_001_id" {
-  value = data.jamfpro_account_groups.jamfpro_account_groups_001_data.id
+output "jamfpro_jamfpro_account_group_001_id" {
+  value = data.jamfpro_account_group.jamfpro_account_group_001_data.id
 }
 
 output "jamfpro_jamfpro_account_groups_001_name" {
-  value = data.jamfpro_account_groups.jamfpro_account_groups_001_data.name
+  value = data.jamfpro_account_group.jamfpro_account_group_001_data.name
 }
--- examples/diskencryptionconfigurations/resource.tf
+++ examples/diskencryptionconfigurations/resource.tf
@@ -1,7 +1,7 @@
 
 // jamfpro Institutional Recovery Key config tf example 
 
-resource "jamfpro_disk_encryption_configurations" "disk_encryption_configuration_01" {
+resource "jamfpro_disk_encryption_configuration" "disk_encryption_configuration_01" {
   name                      = "jamfpro-tf-example-InstitutionalRecoveryKey-config"
   key_type                  = "Institutional" # Or "Individual and Institutional"
   file_vault_enabled_users  = "Management Account" # Or "Current or Next User"
@@ -16,7 +16,7 @@ resource "jamfpro_disk_encryption_configurations" "disk_encryption_configuration
 
 // jamfpro Individual Recovery Key config tf example 
 
-resource "jamfpro_disk_encryption_configurations" "disk_encryption_configuration_02" {
+resource "jamfpro_disk_encryption_configuration" "disk_encryption_configuration_02" {
   name                      = "jamfpro-tf-example-IndividualRecoveryKey-config"
   key_type                  = "Individual" 
   file_vault_enabled_users  = "Management Account" # Or "Current or Next User"
