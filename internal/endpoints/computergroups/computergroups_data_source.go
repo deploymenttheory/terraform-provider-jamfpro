2c435c9cc8afea8c2ee3ae40c6a0ca0c3ffa615a
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-02-28 13:48:32 +0000

    update

--- internal/endpoints/computergroups/computergroups_data_source.go
+++ internal/endpoints/computergroups/computergroups_data_source.go
@@ -9,6 +9,7 @@ import (
 	"github.com/deploymenttheory/go-api-sdk-jamfpro/sdk/jamfpro"
 	"github.com/deploymenttheory/terraform-provider-jamfpro/internal/client"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
@@ -28,106 +29,6 @@ func DataSourceJamfProComputerGroups() *schema.Resource {
 				Computed:    true,
 				Description: "The unique name of the Jamf Pro computer group.",
 			},
-			"is_smart": {
-				Type:        schema.TypeBool,
-				Computed:    true,
-				Description: "Smart or static group.",
-			},
-			"site": {
-				Type:        schema.TypeList,
-				Computed:    true,
-				Description: "Jamf Pro Site-related settings of the computer group.",
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"id": {
-							Type:        schema.TypeInt,
-							Computed:    true,
-							Description: "The ID of the site.",
-						},
-						"name": {
-							Type:        schema.TypeString,
-							Computed:    true,
-							Description: "Name of the site.",
-						},
-					},
-				},
-			},
-			"criteria": {
-				Type:     schema.TypeList,
-				Computed: true,
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"name": {
-							Type:        schema.TypeString,
-							Computed:    true,
-							Description: "Name of the criteria.",
-						},
-						"priority": {
-							Type:        schema.TypeInt,
-							Computed:    true,
-							Description: "The priority of the criterion.",
-						},
-						"and_or": {
-							Type:        schema.TypeString,
-							Computed:    true,
-							Description: "Either 'and' or 'or'.",
-						},
-						"search_type": {
-							Type:        schema.TypeString,
-							Computed:    true,
-							Description: "The type of search operator.",
-						},
-						"value": {
-							Type:        schema.TypeString,
-							Computed:    true,
-							Description: "Criteria search value.",
-						},
-						"opening_paren": {
-							Type:        schema.TypeBool,
-							Computed:    true,
-							Description: "Opening parenthesis flag.",
-						},
-						"closing_paren": {
-							Type:        schema.TypeBool,
-							Computed:    true,
-							Description: "Closing parenthesis flag.",
-						},
-					},
-				},
-			},
-			"computers": {
-				Type:     schema.TypeList,
-				Computed: true,
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"id": {
-							Type:        schema.TypeInt,
-							Computed:    true,
-							Description: "The ID of the computer.",
-						},
-						"name": {
-							Type:        schema.TypeString,
-							Computed:    true,
-							Description: "Name of the computer.",
-						},
-						"mac_address": {
-							Type:        schema.TypeString,
-							Computed:    true,
-							Description: "MAC Address of the computer.",
-						},
-						"alt_mac_address": {
-							Type:        schema.TypeString,
-							Computed:    true,
-							Description: "Alternative MAC Address of the computer.",
-						},
-						"serial_number": {
-							Type:        schema.TypeString,
-							Computed:    true,
-							Description: "Serial number of the computer.",
-						},
-					},
-				},
-			},
 		},
 	}
 }
@@ -145,95 +46,51 @@ func DataSourceJamfProComputerGroups() *schema.Resource {
 // Returns:
 // - diag.Diagnostics: Returns any diagnostics (errors or warnings) encountered during the function's execution.
 func DataSourceJamfProComputerGroupsRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	// Asserts 'meta' as '*client.APIClient'
+	// Initialize API client
 	apiclient, ok := meta.(*client.APIClient)
 	if !ok {
 		return diag.Errorf("error asserting meta as *client.APIClient")
 	}
 	conn := apiclient.Conn
 
-	var group *jamfpro.ResourceComputerGroup
-	var err error
+	// Initialize variables
+	var diags diag.Diagnostics
+	resourceID := d.Id()
 
-	// Check if Name is provided in the data source configuration
-	if v, ok := d.GetOk("name"); ok {
-		groupName, ok := v.(string)
-		if !ok {
-			return diag.Errorf("error asserting 'name' as string")
+	// Convert resourceID from string to int
+	resourceIDInt, err := strconv.Atoi(resourceID)
+	if err != nil {
+		return diag.FromErr(fmt.Errorf("error converting resource ID '%s' to int: %v", resourceID, err))
+	}
+
+	var resource *jamfpro.ResourceComputerGroup
+
+	// Read operation with retry
+	err = retry.RetryContext(ctx, d.Timeout(schema.TimeoutRead), func() *retry.RetryError {
+		var apiErr error
+		resource, apiErr = conn.GetComputerGroupByID(resourceIDInt)
+		if apiErr != nil {
+			// Convert any API error into a retryable error to continue retrying
+			return retry.RetryableError(apiErr)
 		}
-		if groupName != "" {
-			group, err = conn.GetComputerGroupByName(groupName)
-			if err != nil {
-				return diag.FromErr(fmt.Errorf("failed to fetch computer group by name: %v", err))
-			}
-		}
-	} else if v, ok := d.GetOk("id"); ok {
-		groupIDStr, ok := v.(string)
-		if !ok {
-			return diag.Errorf("error asserting 'id' as string")
-		}
-		groupID, err := strconv.Atoi(groupIDStr)
-		if err != nil {
-			return diag.FromErr(fmt.Errorf("failed to parse computer group ID: %v", err))
-		}
-		group, err = conn.GetComputerGroupByID(groupID)
-		if err != nil {
-			return diag.FromErr(fmt.Errorf("failed to fetch computer group by ID: %v", err))
+		// Successfully read the resource, exit the retry loop
+		return nil
+	})
+
+	if err != nil {
+		// Handle the final error after all retries have been exhausted
+		return diag.FromErr(fmt.Errorf("failed to read Jamf Pro Printer with ID '%s' after retries: %v", resourceID, err))
+	}
+
+	// Check if resource data exists and set the Terraform state
+	if resource != nil {
+		d.SetId(resourceID) // Confirm the ID in the Terraform state
+		if err := d.Set("name", resource.Name); err != nil {
+			diags = append(diags, diag.FromErr(fmt.Errorf("error setting 'name' for Jamf Pro Printer with ID '%s': %v", resourceID, err))...)
 		}
 	} else {
-		return diag.Errorf("Either 'name' or 'id' must be provided")
+		d.SetId("") // Data not found, unset the ID in the Terraform state
 	}
 
-	// Set the data source attributes using the fetched data
-	d.SetId(fmt.Sprintf("%d", group.ID))
-
-	if err := d.Set("name", group.Name); err != nil {
-		return diag.FromErr(fmt.Errorf("error setting 'name': %v", err))
-	}
-	if err := d.Set("is_smart", group.IsSmart); err != nil {
-		return diag.FromErr(fmt.Errorf("error setting 'is_smart': %v", err))
-	}
-	if err := d.Set("site", []interface{}{map[string]interface{}{
-		"id":   group.Site.ID,
-		"name": group.Site.Name,
-	}}); err != nil {
-		return diag.FromErr(fmt.Errorf("error setting 'site': %v", err))
-	}
-
-	// Set the criteria
-	criteriaList := make([]interface{}, len(group.Criteria.Criterion))
-	for i, crit := range group.Criteria.Criterion {
-		criteriaMap := map[string]interface{}{
-			"name":          crit.Name,
-			"priority":      crit.Priority,
-			"and_or":        crit.AndOr,
-			"search_type":   crit.SearchType,
-			"value":         crit.Value,
-			"opening_paren": crit.OpeningParen,
-			"closing_paren": crit.ClosingParen,
-		}
-		criteriaList[i] = criteriaMap
-	}
-
-	if err := d.Set("criteria", criteriaList); err != nil {
-		return diag.FromErr(fmt.Errorf("error setting 'criteria': %v", err))
-	}
-
-	// Set the computers
-	computersList := make([]interface{}, len(group.Computers))
-	for i, computer := range group.Computers {
-		computersList[i] = map[string]interface{}{
-			"id":              computer.ID,
-			"name":            computer.Name,
-			"mac_address":     computer.MacAddress,
-			"alt_mac_address": computer.AltMacAddress,
-			"serial_number":   computer.SerialNumber,
-		}
-	}
-
-	if err := d.Set("computers", computersList); err != nil {
-		return diag.FromErr(fmt.Errorf("error setting 'computers': %v", err))
-	}
-
-	return nil
+	return diags
 }
