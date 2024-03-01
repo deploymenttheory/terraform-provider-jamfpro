c7e5eca7d680af148c3cf3ad249f81ded49cc40b
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-03-01 16:40:05 +0000

    Update resource read functions

--- internal/endpoints/computergroups/computergroups_resource.go
+++ internal/endpoints/computergroups/computergroups_resource.go
@@ -303,54 +303,56 @@ func ResourceJamfProComputerGroupsRead(ctx context.Context, d *schema.ResourceDa
 		return diag.FromErr(fmt.Errorf("failed to read Jamf Pro Computer Group with ID '%s' after retries: %v", resourceID, err))
 	}
 
-	// Set attributes in the Terraform state
-	if err := d.Set("name", resource.Name); err != nil {
-		diags = append(diags, diag.FromErr(err)...)
-	}
-	if err := d.Set("is_smart", resource.IsSmart); err != nil {
-		diags = append(diags, diag.FromErr(err)...)
-	}
-
-	site := map[string]interface{}{
-		"id":   resource.Site.ID,
-		"name": resource.Site.Name,
-	}
-	if err := d.Set("site", []interface{}{site}); err != nil {
-		diags = append(diags, diag.FromErr(err)...)
-	}
-
-	// Set the criteria
-	criteriaList := make([]interface{}, len(resource.Criteria.Criterion))
-	for i, crit := range resource.Criteria.Criterion {
-		criteriaMap := map[string]interface{}{
-			"name":          crit.Name,
-			"priority":      crit.Priority,
-			"and_or":        crit.AndOr,
-			"search_type":   crit.SearchType,
-			"value":         crit.Value,
-			"opening_paren": crit.OpeningParen,
-			"closing_paren": crit.ClosingParen,
+	// Update the Terraform state with the fetched data
+	if resource != nil {
+		if err := d.Set("name", resource.Name); err != nil {
+			diags = append(diags, diag.FromErr(err)...)
 		}
-		criteriaList[i] = criteriaMap
-	}
-	if err := d.Set("criteria", criteriaList); err != nil {
-		diags = append(diags, diag.FromErr(err)...)
-	}
-
-	// Set the computers
-	computersList := make([]interface{}, len(resource.Computers))
-	for i, comp := range resource.Computers {
-		computerMap := map[string]interface{}{
-			"id":              comp.ID,
-			"name":            comp.Name,
-			"mac_address":     comp.MacAddress,
-			"alt_mac_address": comp.AltMacAddress,
-			"serial_number":   comp.SerialNumber,
+		if err := d.Set("is_smart", resource.IsSmart); err != nil {
+			diags = append(diags, diag.FromErr(err)...)
 		}
-		computersList[i] = computerMap
-	}
-	if err := d.Set("computers", computersList); err != nil {
-		diags = append(diags, diag.FromErr(err)...)
+
+		site := map[string]interface{}{
+			"id":   resource.Site.ID,
+			"name": resource.Site.Name,
+		}
+		if err := d.Set("site", []interface{}{site}); err != nil {
+			diags = append(diags, diag.FromErr(err)...)
+		}
+
+		// Set the criteria
+		criteriaList := make([]interface{}, len(resource.Criteria.Criterion))
+		for i, crit := range resource.Criteria.Criterion {
+			criteriaMap := map[string]interface{}{
+				"name":          crit.Name,
+				"priority":      crit.Priority,
+				"and_or":        crit.AndOr,
+				"search_type":   crit.SearchType,
+				"value":         crit.Value,
+				"opening_paren": crit.OpeningParen,
+				"closing_paren": crit.ClosingParen,
+			}
+			criteriaList[i] = criteriaMap
+		}
+		if err := d.Set("criteria", criteriaList); err != nil {
+			diags = append(diags, diag.FromErr(err)...)
+		}
+
+		// Set the computers
+		computersList := make([]interface{}, len(resource.Computers))
+		for i, comp := range resource.Computers {
+			computerMap := map[string]interface{}{
+				"id":              comp.ID,
+				"name":            comp.Name,
+				"mac_address":     comp.MacAddress,
+				"alt_mac_address": comp.AltMacAddress,
+				"serial_number":   comp.SerialNumber,
+			}
+			computersList[i] = computerMap
+		}
+		if err := d.Set("computers", computersList); err != nil {
+			diags = append(diags, diag.FromErr(err)...)
+		}
 	}
 
 	return diags
