3a399d2c3eb023c8a9d9361d13ffa44111db1cbc
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-03-03 22:51:50 +0000

    Refactor package creation and upload process

--- internal/endpoints/packages/packages_object.go
+++ internal/endpoints/packages/packages_object.go
@@ -7,11 +7,37 @@ import (
 )
 
 // constructJamfProPackage constructs a ResourcePackage object from the provided schema data.
-func constructJamfProPackageCreate(d *schema.ResourceData) (*jamfpro.ResourcePackage, error) {
+func constructJamfProPackage(d *schema.ResourceData) (*jamfpro.ResourcePackage, error) {
 
 	packageResource := &jamfpro.ResourcePackage{
 		Name:                       d.Get("name").(string),
 		Filename:                   d.Get("filename").(string),
+		Category:                   d.Get("category").(string),
+		Info:                       d.Get("info").(string),
+		Notes:                      d.Get("notes").(string),
+		Priority:                   d.Get("priority").(int),
+		RebootRequired:             d.Get("reboot_required").(bool),
+		FillUserTemplate:           d.Get("fill_user_template").(bool),
+		FillExistingUsers:          d.Get("fill_existing_users").(bool),
+		BootVolumeRequired:         d.Get("boot_volume_required").(bool),
+		AllowUninstalled:           d.Get("allow_uninstalled").(bool),
+		OSRequirements:             d.Get("os_requirements").(string),
+		RequiredProcessor:          d.Get("required_processor").(string),
+		SwitchWithPackage:          d.Get("switch_with_package").(string),
+		InstallIfReportedAvailable: d.Get("install_if_reported_available").(bool),
+		ReinstallOption:            d.Get("reinstall_option").(string),
+		TriggeringFiles:            d.Get("triggering_files").(string),
+		SendNotification:           d.Get("send_notification").(bool),
+	}
+
+	return packageResource, nil
+}
+
+// constructJamfProPackage constructs a ResourcePackage object from the provided schema data and filename.
+func constructJamfProPackageCreate(d *schema.ResourceData, filename string) (*jamfpro.ResourcePackage, error) {
+	packageResource := &jamfpro.ResourcePackage{
+		Name:                       d.Get("name").(string),
+		Filename:                   filename, // Use the provided filename here
 		Category:                   d.Get("category").(string),
 		Info:                       d.Get("info").(string),
 		Notes:                      d.Get("notes").(string),
