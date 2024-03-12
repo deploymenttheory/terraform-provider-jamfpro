1a7099d3eaa488ab1343e1d5fb171c5c3481747c
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-03-12 14:40:19 +0000

    Update resource.tf with optional category and fill_user_template changes

--- examples/packages/resource.tf
+++ examples/packages/resource.tf
@@ -2,13 +2,13 @@
 resource "jamfpro_package" "jamfpro_package_001" {
   name                          = "tf-example-package-your-package-name"
   package_file_path             = file("path/to/your.pkg")
-  category                      = "" // Optional
+  category                      = "" // Optional / can be explitly set or a reference to a jamfpro_category resource e.g jamfpro_category.jamfpro_category_001.name
   info                          = "tf package deployment for demonstration"
   notes                         = "This package is used for Terraform provider documentation example."
   priority                      = 10
   reboot_required               = false
-  fill_user_template            = false
-  fill_existing_users           = false
+  fill_user_template            = false // Optional for .dmg files only
+  fill_existing_users           = false // Optional for .dmg files only
   boot_volume_required          = false
   allow_uninstalled             = false
   os_requirements               = "macOS 10.15.7, macOS 11.1"
