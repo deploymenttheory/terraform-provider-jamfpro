ae078e9dfce8b3e0b9a458cd358b19c399e9be96
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-04-15 15:50:26 +0100

    Refactor state handling in ResourceJamfProAllowedFileExtensionRead function

--- internal/endpoints/allowedfileextensions/allowedfileextensions_resource.go
+++ internal/endpoints/allowedfileextensions/allowedfileextensions_resource.go
@@ -5,7 +5,6 @@ import (
 	"context"
 	"fmt"
 	"strconv"
-	"strings"
 	"time"
 
 	"github.com/deploymenttheory/terraform-provider-jamfpro/internal/client"
@@ -137,33 +136,18 @@ func ResourceJamfProAllowedFileExtensionRead(ctx context.Context, d *schema.Reso
 	resource, err := conn.GetAllowedFileExtensionByID(resourceIDInt)
 
 	if err != nil {
-		// Skip resource state removal if this is a create operation
-		if !d.IsNewResource() {
-			// If the error is a "not found" error, remove the resource from the state
-			if strings.Contains(err.Error(), "404") || strings.Contains(err.Error(), "410") {
-				d.SetId("") // Remove the resource from Terraform state
-				return diag.Diagnostics{
-					{
-						Severity: diag.Warning,
-						Summary:  "Resource not found",
-						Detail:   fmt.Sprintf("Jamf Pro Allowed File Extension resource with ID '%s' was not found and has been removed from the Terraform state.", resourceID),
-					},
-				}
-			}
-		}
-		// For other errors, or if this is a create operation, return a diagnostic error
-		return diag.FromErr(err)
+		// Handle not found error or other errors
+		return common.HandleResourceNotFoundError(err, d)
 	}
 
-	// Update the Terraform state with the fetched data
-	if err := d.Set("id", resourceID); err != nil {
-		diags = append(diags, diag.FromErr(err)...)
-	}
-	if err := d.Set("extension", resource.Extension); err != nil {
-		diags = append(diags, diag.FromErr(err)...)
-	}
+	// Update the Terraform state with the fetched data from the resource
+	diags = updateTerraformState(d, resource)
 
-	return diags
+	// Handle any errors and return diagnostics
+	if len(diags) > 0 {
+		return diags
+	}
+	return nil
 }
 
 // ResourceJamfProAllowedFileExtensionUpdate handles the update operation for an AllowedFileExtension resource in Terraform.
---
+++ internal/endpoints/allowedfileextensions/allowedfileextensions_state.go
@@ -0,0 +1,20 @@
+// allowedfileextensions_state.go
+package allowedfileextensions
+
+import (
+	"github.com/deploymenttheory/go-api-sdk-jamfpro/sdk/jamfpro"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+// updateTerraformState updates the Terraform state with the latest Allowed File Extension information from the Jamf Pro API.
+func updateTerraformState(d *schema.ResourceData, resource *jamfpro.ResourceAllowedFileExtension) diag.Diagnostics {
+
+	var diags diag.Diagnostics
+
+	if err := d.Set("extension", resource.Extension); err != nil {
+		diags = append(diags, diag.FromErr(err)...)
+	}
+
+	return diags
+}
