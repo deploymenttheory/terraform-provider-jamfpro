60ab6ab216e3913bc35c2079453242d8e4dc4a55
Joseph Little <joelittle11@live.com>
2024-04-16 15:34:44 +0100

    script standardised

--- internal/endpoints/policies/policies_resource.go
+++ internal/endpoints/policies/policies_resource.go
@@ -311,22 +311,7 @@ func ResourceJamfProPolicies() *schema.Resource {
 				Type:        schema.TypeList,
 				Required:    true,
 				Description: "Jamf Pro Site-related settings of the policy.",
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"id": {
-							Type:        schema.TypeInt,
-							Optional:    true,
-							Description: "Jamf Pro Site ID. Value defaults to -1 aka not used.",
-							Default:     -1,
-						},
-						"name": {
-							Type:        schema.TypeString,
-							Optional:    true,
-							Description: "Jamf Pro Site Name. Value defaults to 'None' aka not used",
-							Default:     "None",
-						},
-					},
-				},
+				Elem:        sharedschemas.GetSharedSchemaSite(),
 			},
 			"scope": {
 				Type:        schema.TypeList,
@@ -360,76 +345,7 @@ func ResourceJamfProPolicies() *schema.Resource {
 				Type:        schema.TypeList,
 				Optional:    true,
 				Description: "Scripts settings of the policy.",
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"script": { // Unnecessary branching
-							Type:        schema.TypeList,
-							Optional:    true,
-							Description: "Details of the scripts.",
-							Elem: &schema.Resource{
-								Schema: map[string]*schema.Schema{
-									"id": {
-										Type:        schema.TypeInt,
-										Optional:    true,
-										Description: "Unique identifier of the script.",
-									},
-									"name": {
-										Type:        schema.TypeString,
-										Optional:    true,
-										Description: "Name of the script.",
-									},
-									"priority": {
-										Type:         schema.TypeString,
-										Optional:     true,
-										Description:  "Execution priority of the script.",
-										ValidateFunc: validation.StringInSlice([]string{"Before", "After"}, false),
-										Default:      "After",
-									},
-									"parameter4": {
-										Type:        schema.TypeString,
-										Optional:    true,
-										Description: "Custom parameter 4 for the script.",
-									},
-									"parameter5": {
-										Type:        schema.TypeString,
-										Optional:    true,
-										Description: "Custom parameter 5 for the script.",
-									},
-									"parameter6": {
-										Type:        schema.TypeString,
-										Optional:    true,
-										Description: "Custom parameter 6 for the script.",
-									},
-									"parameter7": {
-										Type:        schema.TypeString,
-										Optional:    true,
-										Description: "Custom parameter 7 for the script.",
-									},
-									"parameter8": {
-										Type:        schema.TypeString,
-										Optional:    true,
-										Description: "Custom parameter 8 for the script.",
-									},
-									"parameter9": {
-										Type:        schema.TypeString,
-										Optional:    true,
-										Description: "Custom parameter 9 for the script.",
-									},
-									"parameter10": {
-										Type:        schema.TypeString,
-										Optional:    true,
-										Description: "Custom parameter 10 for the script.",
-									},
-									"parameter11": {
-										Type:        schema.TypeString,
-										Optional:    true,
-										Description: "Custom parameter 11 for the script.",
-									},
-								},
-							},
-						},
-					},
-				},
+				Elem:        getSharedSchemaScript(),
 			},
 			"printers": {
 				Type:        schema.TypeList,
---
+++ internal/endpoints/policies/policies_schema_script.go
@@ -0,0 +1,71 @@
+package policies
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+)
+
+func getSharedSchemaScript() *schema.Resource {
+	out := &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"id": {
+				Type:        schema.TypeInt,
+				Optional:    true,
+				Description: "Unique identifier of the script.",
+			},
+			// "name": {
+			// 	Type:        schema.TypeString,
+			// 	Optional:    true,
+			// 	Description: "Name of the script.",
+			// }, // Do we need this?
+			"priority": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				Description:  "Execution priority of the script.",
+				ValidateFunc: validation.StringInSlice([]string{"Before", "After"}, false),
+				Default:      "After",
+			},
+			"parameter4": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Custom parameter 4 for the script.",
+			},
+			"parameter5": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Custom parameter 5 for the script.",
+			},
+			"parameter6": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Custom parameter 6 for the script.",
+			},
+			"parameter7": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Custom parameter 7 for the script.",
+			},
+			"parameter8": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Custom parameter 8 for the script.",
+			},
+			"parameter9": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Custom parameter 9 for the script.",
+			},
+			"parameter10": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Custom parameter 10 for the script.",
+			},
+			"parameter11": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Custom parameter 11 for the script.",
+			},
+		},
+	}
+	return out
+}
