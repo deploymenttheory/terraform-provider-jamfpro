86bc9100fe788bf04ed5d69c7bfa0d162ac1d279
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-05-26 10:48:23 +0100

    feat: Refactor ConstructSharedResourceSite to use pointer to jamfpro.SharedResourceSite

--- internal/endpoints/common/constructobject/constructobject.go
+++ internal/endpoints/common/constructobject/constructobject.go
@@ -5,20 +5,20 @@ import "github.com/deploymenttheory/go-api-sdk-jamfpro/sdk/jamfpro"
 
 // ConstructSharedResourceSite constructs a SharedResourceSite object from the provided schema data,
 // setting default values if none are presented.
-func ConstructSharedResourceSite(data []interface{}) jamfpro.SharedResourceSite {
+func ConstructSharedResourceSite(data []interface{}) *jamfpro.SharedResourceSite {
 	// Check if 'site' data is provided and non-empty
 	if len(data) > 0 && data[0] != nil {
 		site := data[0].(map[string]interface{})
 
 		// Return the 'site' object with data from the schema
-		return jamfpro.SharedResourceSite{
+		return &jamfpro.SharedResourceSite{
 			ID:   site["id"].(int),
 			Name: site["name"].(string),
 		}
 	}
 
 	// Return default 'site' values if no data is provided or it is empty
-	return jamfpro.SharedResourceSite{
+	return &jamfpro.SharedResourceSite{
 		ID:   -1,     // Default ID
 		Name: "None", // Default name
 	}
