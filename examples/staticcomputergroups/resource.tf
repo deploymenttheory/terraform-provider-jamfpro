75e6780638cf125caca64247b0f9c90187fd9479
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-05-31 16:21:05 +0100

    updated examples

--- examples/staticcomputergroups/resource.tf
+++ examples/staticcomputergroups/resource.tf
@@ -1,6 +1,7 @@
 resource "jamfpro_static_computer_group" "jamfpro_static_computer_group_001" {
   name = "Example Static Computer Group"
 
+  # Optional: Specify site details
   site {
     id   = 123  # Replace with the actual site ID
     name = "Site Name"  # Replace with the actual site name
