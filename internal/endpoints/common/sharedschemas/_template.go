864ddc25e101ca05f9143fa6e7a10d1581c32ab2
Joseph Little <joelittle11@live.com>
2024-06-23 11:30:44 +0100

    standing up contructors & stators for shared schemas

--- internal/endpoints/common/sharedschemas/_template.go
+++
@@ -1,9 +0,0 @@
-package sharedschemas
-
-import "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-
-func GetSharedSchemaNAME() *schema.Resource {
-	out := &schema.Resource{}
-
-	return out
-}
