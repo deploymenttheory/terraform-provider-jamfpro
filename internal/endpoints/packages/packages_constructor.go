814238e3108c97e81b30c72c79e2e1f1936b3eee
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-06-24 22:16:16 +0100

    Refactor package resource to extract filename from full path in schema

--- internal/endpoints/packages/packages_constructor.go
+++ internal/endpoints/packages/packages_constructor.go
@@ -13,33 +13,42 @@ import (
 
 // constructJamfProPackageCreate constructs a ResourcePackage object from the provided schema data.
 // It extracts the filename from the full path provided in the schema and uses it for the FileName field.
+// constructJamfProPackageCreate constructs a ResourcePackage object from the provided schema data.
+// It extracts the filename from the full path provided in the schema and uses it for the FileName field.
 func constructJamfProPackageCreate(d *schema.ResourceData) (*jamfpro.ResourcePackage, error) {
 	// Use filepath.Base to extract just the filename from the full path
 	fullPath := d.Get("package_file_path").(string)
 	fileName := filepath.Base(fullPath)
 
-	// Get the category from the schema, and set it to "-1" if it's empty
-	// 'Unknown' is the valid default request value for the category field when none is set
-	// Jamf API returns "No category assigned" for the same field. But this is not a valid
-	// request value. Why!!!! >_<
-	category := d.Get("category_id").(string)
-	if category == "" {
-		category = "-1"
-	}
-
 	// Construct the ResourcePackage struct from the Terraform schema data
 	resource := &jamfpro.ResourcePackage{
 		PackageName:          d.Get("package_name").(string),
 		FileName:             fileName,
-		CategoryID:           category,
+		CategoryID:           d.Get("category_id").(string),
+		Info:                 d.Get("info").(string),
+		Notes:                d.Get("notes").(string),
 		Priority:             d.Get("priority").(int),
+		OSRequirements:       d.Get("os_requirements").(string),
 		FillUserTemplate:     BoolPtr(d.Get("fill_user_template").(bool)),
+		Indexed:              BoolPtr(d.Get("indexed").(bool)),
+		FillExistingUsers:    BoolPtr(d.Get("fill_existing_users").(bool)),
+		SWU:                  BoolPtr(d.Get("swu").(bool)),
 		RebootRequired:       BoolPtr(d.Get("reboot_required").(bool)),
+		SelfHealNotify:       BoolPtr(d.Get("self_heal_notify").(bool)),
+		SelfHealingAction:    d.Get("self_healing_action").(string),
 		OSInstall:            BoolPtr(d.Get("os_install").(bool)),
+		SerialNumber:         d.Get("serial_number").(string),
+		ParentPackageID:      d.Get("parent_package_id").(string),
+		BasePath:             d.Get("base_path").(string),
 		SuppressUpdates:      BoolPtr(d.Get("suppress_updates").(bool)),
+		IgnoreConflicts:      BoolPtr(d.Get("ignore_conflicts").(bool)),
 		SuppressFromDock:     BoolPtr(d.Get("suppress_from_dock").(bool)),
 		SuppressEula:         BoolPtr(d.Get("suppress_eula").(bool)),
 		SuppressRegistration: BoolPtr(d.Get("suppress_registration").(bool)),
+		InstallLanguage:      d.Get("install_language").(string),
+		OSInstallerVersion:   d.Get("os_installer_version").(string),
+		Manifest:             d.Get("manifest").(string),
+		ManifestFileName:     d.Get("manifest_file_name").(string),
 	}
 
 	// Serialize and pretty-print the Network Segment object as JSON for logging
