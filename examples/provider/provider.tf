389518f80d58cc43d3243576184794ae43342cf2
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-07-03 11:50:32 +0100

    example update for provider

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -2,56 +2,95 @@ terraform {
   required_providers {
     jamfpro = {
       source  = "deploymenttheory/jamfpro"
-      version = "~> 0.0.43"
+      version = "~> 0.1.4"
     }
   }
 }
 
 provider "jamfpro" {
-  jamf_instance_fqdn          = var.jamfpro_instance_name
-  auth_method =               "oauth2" // basic
-  client_id                   = var.jamfpro_client_id
-  client_secret               = var.jamfpro_client_secret
-  # basic_auth_username = ""
-  # basic_auth_password = ""
-  log_level                   = "debug" # or "debug", "info", "none" depending on the desired verbosity of the http client
-  log_output_format           = "console" # or "JSON" for JSON format
-  log_console_separator       = " " # Separator character for console log output
-  log_export_path             = "/path/to/log"
-  export_logs                 = false
-  hide_sensitive_data         = true # Hides sensitive data in logs
-  token_refresh_buffer_period_seconds = 5 # minutes
-  jamf_load_balancer_lock     = true
-  custom_cookies = {
-    name = "jpro-ingress"
-    value = "value"
-  }
-  mandatory_request_delay_milliseconds = 100
+  jamfpro_instance_fqdn                = var.jamfpro_instance_fqdn
+  auth_method                          = var.jamfpro_auth_method
+  client_id                            = var.jamfpro_client_id
+  client_secret                        = var.jamfpro_client_secret
+  enable_client_sdk_logs               = var.enable_client_sdk_logs
+  client_sdk_log_export_path           = var.client_sdk_log_export_path
+  hide_sensitive_data                  = var.jamfpro_hide_sensitive_data
+  jamfpro_load_balancer_lock           = var.jamfpro_jamf_load_balancer_lock
+  token_refresh_buffer_period_seconds  = var.jamfpro_token_refresh_buffer_period_seconds
+  mandatory_request_delay_milliseconds = var.jamfpro_mandatory_request_delay_milliseconds
+  # basic_auth_username           = "bearer-test"
+  # basic_auth_password           = "bearer-test"
 }
 
-variable "jamfpro_instance_name" {
-  description = "Jamf Pro Instance name."
+variable "jamfpro_instance_fqdn" {
+  description = "The Jamf Pro FQDN (fully qualified domain name). Example: https://mycompany.jamfcloud.com"
+  default     = ""
+}
+
+variable "jamfpro_auth_method" {
+  description = "Auth method chosen for Jamf. Options are 'basic' or 'oauth2'."
+  sensitive   = true
   default     = ""
 }
 
 variable "jamfpro_client_id" {
-  description = "Jamf Pro Client ID for authentication."
+  description = "The Jamf Pro Client ID for authentication."
+  sensitive   = true
   default     = ""
 }
 
 variable "jamfpro_client_secret" {
-  description = "Jamf Pro Client Secret for authentication."
+  description = "The Jamf Pro Client Secret for authentication."
   sensitive   = true
   default     = ""
 }
 
-variable "jamfpro_username" {
-  description = "Jamf Pro username used for authentication."
+variable "jamfpro_basic_auth_username" {
+  description = "The Jamf Pro username used for authentication."
   default     = ""
 }
 
-variable "jamfpro_password" {
-  description = "Jamf Pro password used for authentication."
+variable "jamfpro_basic_auth_password" {
+  description = "The Jamf Pro password used for authentication."
   sensitive   = true
   default     = ""
+}
+
+variable "enable_client_sdk_logs" {
+  description = "Enable client SDK logs."
+  default     = false
+}
+
+variable "client_sdk_log_export_path" {
+  description = "Specify the path to export http client logs to."
+  default     = ""
+}
+
+variable "jamfpro_hide_sensitive_data" {
+  description = "Define whether sensitive fields should be hidden in logs."
+  default     = true
+}
+
+variable "jamfpro_custom_cookies" {
+  description = "Custom cookies for the HTTP client."
+  type = list(object({
+    name  = string
+    value = string
+  }))
+  default = []
+}
+
+variable "jamfpro_jamf_load_balancer_lock" {
+  description = "Programmatically determines all available web app members in the load balancer and locks all instances of httpclient to the app for faster executions."
+  default     = true
+}
+
+variable "jamfpro_token_refresh_buffer_period_seconds" {
+  description = "The buffer period in seconds for token refresh."
+  default     = 300
+}
+
+variable "jamfpro_mandatory_request_delay_milliseconds" {
+  description = "A mandatory delay after each request before returning to reduce high volume of requests in a short time."
+  default     = 100
 }
