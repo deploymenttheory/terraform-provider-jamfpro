733abeb18f5018ce3a91c55d1288e5c628ed7ba1
Harry Seeber <harry.seeber@faire.com>
2024-07-25 21:01:00 -0600

    allow self service category display/feature changes without force new

--- internal/resources/macosconfigurationprofilesplist/macosconfigurationprofilesplist_resource.go
+++ internal/resources/macosconfigurationprofilesplist/macosconfigurationprofilesplist_resource.go
@@ -180,14 +180,12 @@ func ResourceJamfProMacOSConfigurationProfilesPlist() *schema.Resource {
 									},
 									"display_in": {
 										Type:        schema.TypeBool,
-										ForceNew:    true,
 										Description: "Display this profile in this category?",
 										Required:    true,
 									},
 									"feature_in": {
 										Type:        schema.TypeBool,
 										Description: "Feature this profile in this category?",
-										ForceNew:    true,
 										Required:    true,
 									},
 								},
