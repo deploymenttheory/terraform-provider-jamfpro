d167d32bf2935bf20ee42c6efc448ec435a56e0b
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-09-09 06:35:24 +0100

    Refactor normalizeScript function to replace CRLF with LF and trim trailing newlines

--- internal/resources/computerextensionattributes/normalize.go
+++ internal/resources/computerextensionattributes/normalize.go
@@ -2,6 +2,9 @@ package computerextensionattributes
 
 import "strings"
 
+// normalizeScript normalizes a script by replacing all CRLF with LF and trimming trailing newlines
 func normalizeScript(script string) string {
-	return strings.TrimRight(script, "\n")
+	normalized := strings.Replace(script, "\r\n", "\n", -1)
+
+	return strings.TrimRight(normalized, "\n")
 }
