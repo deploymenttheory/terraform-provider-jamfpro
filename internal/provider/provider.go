0928ccc684dfe034b1933bedeb4a80c9a8f710a8
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-09-17 13:38:55 +0100

    feat(provider): comment out managed software update resource in provider configuration

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -34,7 +34,6 @@ import (
 	"github.com/deploymenttheory/terraform-provider-jamfpro/internal/resources/filesharedistributionpoints"
 	"github.com/deploymenttheory/terraform-provider-jamfpro/internal/resources/macosconfigurationprofilesplist"
 	"github.com/deploymenttheory/terraform-provider-jamfpro/internal/resources/macosconfigurationprofilesplistgenerator"
-	"github.com/deploymenttheory/terraform-provider-jamfpro/internal/resources/managedsoftwareupdates"
 	"github.com/deploymenttheory/terraform-provider-jamfpro/internal/resources/mobiledeviceconfigurationprofilesplist"
 	"github.com/deploymenttheory/terraform-provider-jamfpro/internal/resources/networksegments"
 	"github.com/deploymenttheory/terraform-provider-jamfpro/internal/resources/packages"
@@ -395,18 +394,18 @@ func Provider() *schema.Provider {
 			"jamfpro_network_segment":                             networksegments.ResourceJamfProNetworkSegments(),
 			"jamfpro_macos_configuration_profile_plist":           macosconfigurationprofilesplist.ResourceJamfProMacOSConfigurationProfilesPlist(),
 			"jamfpro_macos_configuration_profile_plist_generator": macosconfigurationprofilesplistgenerator.ResourceJamfProMacOSConfigurationProfilesPlistGenerator(),
-			"jamfpro_managed_software_update":                     managedsoftwareupdates.ResourceJamfProManagedSoftwareUpdate(),
-			"jamfpro_mobile_device_configuration_profile_plist":   mobiledeviceconfigurationprofilesplist.ResourceJamfProMobileDeviceConfigurationProfilesPlist(),
-			"jamfpro_package":                                     packages.ResourceJamfProPackages(),
-			"jamfpro_policy":                                      policies.ResourceJamfProPolicies(),
-			"jamfpro_printer":                                     printers.ResourceJamfProPrinters(),
-			"jamfpro_script":                                      scripts.ResourceJamfProScripts(),
-			"jamfpro_site":                                        sites.ResourceJamfProSites(),
-			"jamfpro_smart_computer_group":                        smartcomputergroups.ResourceJamfProSmartComputerGroups(),
-			"jamfpro_static_computer_group":                       staticcomputergroups.ResourceJamfProStaticComputerGroups(),
-			"jamfpro_restricted_software":                         restrictedsoftware.ResourceJamfProRestrictedSoftwares(),
-			"jamfpro_user_group":                                  usergroups.ResourceJamfProUserGroups(),
-			"jamfpro_webhook":                                     webhooks.ResourceJamfProWebhooks(),
+			//"jamfpro_managed_software_update":                     managedsoftwareupdates.ResourceJamfProManagedSoftwareUpdate(),
+			"jamfpro_mobile_device_configuration_profile_plist": mobiledeviceconfigurationprofilesplist.ResourceJamfProMobileDeviceConfigurationProfilesPlist(),
+			"jamfpro_package":               packages.ResourceJamfProPackages(),
+			"jamfpro_policy":                policies.ResourceJamfProPolicies(),
+			"jamfpro_printer":               printers.ResourceJamfProPrinters(),
+			"jamfpro_script":                scripts.ResourceJamfProScripts(),
+			"jamfpro_site":                  sites.ResourceJamfProSites(),
+			"jamfpro_smart_computer_group":  smartcomputergroups.ResourceJamfProSmartComputerGroups(),
+			"jamfpro_static_computer_group": staticcomputergroups.ResourceJamfProStaticComputerGroups(),
+			"jamfpro_restricted_software":   restrictedsoftware.ResourceJamfProRestrictedSoftwares(),
+			"jamfpro_user_group":            usergroups.ResourceJamfProUserGroups(),
+			"jamfpro_webhook":               webhooks.ResourceJamfProWebhooks(),
 		},
 	}
 
