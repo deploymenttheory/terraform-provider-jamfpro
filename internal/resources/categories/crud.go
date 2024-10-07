351dd69db066f9a4fb63ee7cfbcafebdd8a380f7
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-10-07 19:45:40 +0100

    modified:   internal/resources/categories/crud.go

--- internal/resources/categories/crud.go
+++ internal/resources/categories/crud.go
@@ -2,23 +2,48 @@ package categories
 
 import (
 	"context"
+	"fmt"
+	"sync"
 
 	"github.com/deploymenttheory/go-api-sdk-jamfpro/sdk/jamfpro"
 	"github.com/deploymenttheory/terraform-provider-jamfpro/internal/resources/common"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
-// create is responsible for creating a new Jamf Pro Category in the remote system.
+// Create requires a mutex need to lock Create requests during parallel runs
+var mu sync.Mutex
+
 func create(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	return common.Create(
-		ctx,
-		d,
-		meta,
-		construct,
-		meta.(*jamfpro.Client).CreateCategory,
-		readNoCleanup,
-	)
+	client := meta.(*jamfpro.Client)
+	var diags diag.Diagnostics
+
+	// Lock the mutex to ensure only one profile plust create can run this function at a time
+	mu.Lock()
+	defer mu.Unlock()
+	resource, err := construct(d)
+	if err != nil {
+		return diag.FromErr(fmt.Errorf("failed to construct Jamf Pro macOS Configuration Profile: %v", err))
+	}
+
+	var creationResponse *jamfpro.ResponseCategoryCreateAndUpdate
+	err = retry.RetryContext(ctx, d.Timeout(schema.TimeoutCreate), func() *retry.RetryError {
+		var apiErr error
+		creationResponse, apiErr = client.CreateCategory(resource)
+		if apiErr != nil {
+			return retry.RetryableError(apiErr)
+		}
+		return nil
+	})
+
+	if err != nil {
+		return diag.FromErr(fmt.Errorf("failed to create Jamf Pro Category '%s' after retries: %v", resource.Name, err))
+	}
+
+	d.SetId(creationResponse.ID)
+
+	return append(diags, readNoCleanup(ctx, d, meta)...)
 }
 
 // read is responsible for reading the current state of a Jamf Pro Category Resource from the remote system.
