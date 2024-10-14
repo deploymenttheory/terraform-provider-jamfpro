a36c0e968be54d297d91a6b01e6952f2776d56b2
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-10-14 22:35:14 +0100

    Refactor mobile device extension attribute resource.tf file

--- examples/resources/jamfpro_mobile_device_extension_attribute/resource.tf
+++ examples/resources/jamfpro_mobile_device_extension_attribute/resource.tf
@@ -1,7 +1,29 @@
+# Pop-up menu Example
+resource "jamfpro_mobile_device_extension_attribute" "popup_menu_example" {
+  name               = "Device Location"
+  description        = "The primary location where this device is used"
+  data_type          = "String"
+  inventory_display  = "User and Location"
+  
+  input_type {
+    type = "Pop-up Menu"
+    popup_choices = [
+      "Head Office",
+      "Branch Office",
+      "Home Office",
+      "Client Site"
+    ]
+  }
+}
+
 # Text Field Example
-resource "jamfpro_mobile_device_extension_attribute" "mobile_device_extension_attribute_text_field_1" {
-  name                   = "tf-example-text-field-ea"
-  description            = "An attribute collected from a text field."
-  data_type              = "STRING"
-  inventory_display_type = "EXTENSION_ATTRIBUTES"
+resource "jamfpro_mobile_device_extension_attribute" "text_field_example" {
+  name               = "User Department"
+  description        = "The department to which the device user belongs"
+  data_type          = "String"
+  inventory_display  = "General"
+  
+  input_type {
+    type = "Text Field"
+  }
 }
