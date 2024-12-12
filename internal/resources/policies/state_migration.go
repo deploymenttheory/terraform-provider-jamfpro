ae80f2c23dd763a7c527b0b0e7b214465cf4e752
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-12-12 12:28:34 +0000

    fix: refactor of state migration logic for Payload/UserInteraction (#517)
    Refactor user interaction schema and update state migration for policy resource

--- internal/resources/policies/state_migration.go
+++ internal/resources/policies/state_migration.go
@@ -6,34 +6,72 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
-// resourcePolicyUserInteractionV0 defines the v0 schema for the user interaction block in the policy resource.
-func resourcePolicyUserInteractionV0() *schema.Resource {
+// resourcePolicyV0 defines the v0 schema for the user interaction block in the policy resource.
+func resourcePolicyV0() *schema.Resource {
 	return &schema.Resource{
 		Schema: map[string]*schema.Schema{
-			"message_start":            {Type: schema.TypeString, Optional: true},
-			"allow_user_to_defer":      {Type: schema.TypeBool, Optional: true, Default: false},
-			"allow_deferral_until_utc": {Type: schema.TypeString, Optional: true},
-			"allow_deferral_minutes":   {Type: schema.TypeInt, Optional: true, Default: 0},
-			"message_finish":           {Type: schema.TypeString, Optional: true},
+			"payloads": {
+				Type:     schema.TypeList,
+				Required: true,
+				MaxItems: 1,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"user_interaction": {
+							Type:     schema.TypeList,
+							MaxItems: 1,
+							Optional: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"message_start": {
+										Type:     schema.TypeString,
+										Optional: true,
+									},
+									"allow_user_to_defer": { // Old field name
+										Type:     schema.TypeBool,
+										Optional: true,
+									},
+									"allow_deferral_until_utc": {
+										Type:     schema.TypeString,
+										Optional: true,
+									},
+									"allow_deferral_minutes": {
+										Type:     schema.TypeInt,
+										Optional: true,
+									},
+									"message_finish": {
+										Type:     schema.TypeString,
+										Optional: true,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
 		},
 	}
 }
 
-// Migration function to handle the upgrade from V0 to V1
-// copies the existing fields and updates the allow_user_to_defer field to allow_users_to_defer
 func upgradePolicyUserInteractionV0toV1(ctx context.Context, rawState map[string]interface{}, meta interface{}) (map[string]interface{}, error) {
-	newState := make(map[string]interface{})
+	if payloads, ok := rawState["payloads"].([]interface{}); ok && len(payloads) > 0 {
+		payload := payloads[0].(map[string]interface{})
+		if userInteractions, ok := payload["user_interaction"].([]interface{}); ok && len(userInteractions) > 0 {
+			userInteraction := userInteractions[0].(map[string]interface{})
 
-	newState["message_start"] = rawState["message_start"]
-	newState["allow_deferral_until_utc"] = rawState["allow_deferral_until_utc"]
-	newState["allow_deferral_minutes"] = rawState["allow_deferral_minutes"]
-	newState["message_finish"] = rawState["message_finish"]
+			// Create a new interaction block with the new field name
+			newInteraction := map[string]interface{}{
+				"message_start":            userInteraction["message_start"],
+				"allow_users_to_defer":     userInteraction["allow_user_to_defer"],
+				"allow_deferral_until_utc": userInteraction["allow_deferral_until_utc"],
+				"allow_deferral_minutes":   userInteraction["allow_deferral_minutes"],
+				"message_finish":           userInteraction["message_finish"],
+			}
 
-	if deferVal, ok := rawState["allow_user_to_defer"]; ok {
-		newState["allow_users_to_defer"] = deferVal
-	} else {
-		newState["allow_users_to_defer"] = false
+			userInteractions[0] = newInteraction
+			payload["user_interaction"] = userInteractions
+			payloads[0] = payload
+			rawState["payloads"] = payloads
+		}
 	}
-
-	return newState, nil
+	return rawState, nil
 }
