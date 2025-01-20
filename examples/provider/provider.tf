393f54c6dfacdb60c430e19e3e524e09ca851c5d
ShocOne <62835948+ShocOne@users.noreply.github.com>
2025-01-20 12:13:13 +0000

    fix(provider): update jamfpro source and version in provider.tf

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     jamfpro = {
-      source  = "terraform.local/local/jamfpro"
-      version = "0.1.0"
+      source  = "deploymenttheory/jamfpro"
+      version = "~> 0.10.0"
     }
   }
 }
