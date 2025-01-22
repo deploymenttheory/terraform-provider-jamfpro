36ada493b1aea8f419a3aa25b4e04ceba38cb2be
ShocOne <62835948+ShocOne@users.noreply.github.com>
2025-01-22 15:39:55 +0000

    fix: refactored logic to define new pattern for data sources

--- internal/resources/deviceenrollments/data_source.go
+++ internal/resources/deviceenrollments/data_source.go
@@ -83,27 +83,39 @@ func DataSourceJamfProDeviceEnrollments() *schema.Resource {
 	}
 }
 
-// dataSourceRead fetches the details of a specific script from Jamf Pro using either its unique Name or its Id.
+// dataSourceRead fetches the details of a device enrollment from Jamf Pro using either its Name or Id.
 func dataSourceRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	client := meta.(*jamfpro.Client)
 
-	resourceID := d.Get("id").(string)
+	id := d.Get("id").(string)
 	name := d.Get("name").(string)
 
-	if resourceID == "" && name == "" {
+	if id != "" && name != "" {
+		return diag.FromErr(fmt.Errorf("please provide either 'id' or 'name', not both"))
+	}
+
+	var getFunc func() (*jamfpro.ResourceDeviceEnrollment, error)
+	var identifier string
+
+	switch {
+	case id != "":
+		getFunc = func() (*jamfpro.ResourceDeviceEnrollment, error) {
+			return client.GetDeviceEnrollmentByID(id)
+		}
+		identifier = id
+	case name != "":
+		getFunc = func() (*jamfpro.ResourceDeviceEnrollment, error) {
+			return client.GetDeviceEnrollmentByName(name)
+		}
+		identifier = name
+	default:
 		return diag.FromErr(fmt.Errorf("either 'id' or 'name' must be provided"))
 	}
 
 	var resource *jamfpro.ResourceDeviceEnrollment
 	err := retry.RetryContext(ctx, d.Timeout(schema.TimeoutRead), func() *retry.RetryError {
 		var apiErr error
-
-		if name != "" {
-			resource, apiErr = client.GetDeviceEnrollmentByName(name)
-		} else {
-			resource, apiErr = client.GetDeviceEnrollmentByID(resourceID)
-		}
-
+		resource, apiErr = getFunc()
 		if apiErr != nil {
 			return retry.RetryableError(apiErr)
 		}
@@ -111,13 +123,7 @@ func dataSourceRead(ctx context.Context, d *schema.ResourceData, meta interface{
 	})
 
 	if err != nil {
-		lookupMethod := "ID"
-		lookupValue := resourceID
-		if name != "" {
-			lookupMethod = "name"
-			lookupValue = name
-		}
-		return diag.FromErr(fmt.Errorf("failed to read Jamf Pro Device Enrollment with %s '%s' after retries: %v", lookupMethod, lookupValue, err))
+		return diag.FromErr(fmt.Errorf("failed to read Jamf Pro Device Enrollment with identifier '%s' after retries: %v", identifier, err))
 	}
 
 	if resource == nil {
