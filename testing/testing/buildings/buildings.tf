065dabf55f71462dac13e29652af61b7daa6a968
Bobby <bobby.james.williams@outlook.com>
2025-03-25 11:58:36 +0000

    polish building and scripts

--- testing/testing/buildings/buildings.tf
+++ testing/testing/buildings/buildings.tf
@@ -2,12 +2,38 @@
 // Buildings
 // ========================================================================== //
 
+// ========================================================================== //
+// Single buildings
 
-resource "jamfpro_building" "building" {
-  name = "tf-testing-local-bw"
+resource "jamfpro_building" "building_min" {
+  name = "tf-testing-min"
 }
 
-resource "jamfpro_building" "building_multiple" {
+resource "jamfpro_building" "building_max" {
+  name = "tf-testing-max"
+  street_address1 = "unit 1"
+  street_address2 = "1 example drive"
+  city = "Jamftown"
+  state_province = "Jamfshire"
+  zip_postal_code = "JM22 5AM"
+  country = "Jamf Republic"
+}
+
+// ========================================================================== //
+// Multiple buildings
+
+resource "jamfpro_building" "building_multiple_max" {
   count = 100
-  name = "tf-testing-local-bw-${count.index}"
+  name = "tf-testing-max-${count.index}"
+  street_address1 = "unit 1"
+  street_address2 = "1 example drive"
+  city = "Jamftown"
+  state_province = "Jamfshire"
+  zip_postal_code = "JM22 5AM"
+  country = "Jamf Republic"
+}
+
+resource "jamfpro_building" "building_multiple_min" {
+  count = 100
+  name = "tf-testing-min-${count.index}"
 }
--- testing/testing/scripts/scripts.tf
+++ testing/testing/scripts/scripts.tf
@@ -1,14 +1,19 @@
 // ========================================================================== //
 // Scripts
+// ========================================================================== //
+
+
+// ========================================================================== //
+// Single scripts
 
 resource "jamfpro_script" "min_script" {
-  name = "tf-testing-local-bw-script-min"
+  name = "tf-testing-script-min"
   script_contents = "script_contents_field"
   priority = "BEFORE"
 }
 
 resource "jamfpro_script" "max_script" {
-  name = "tf-testing-local-bw-script-max"
+  name = "tf-testing-bw-script-max"
   category_id = "9"
   info = "info_field"
   notes = "notes_field"
@@ -24,17 +29,31 @@ resource "jamfpro_script" "max_script" {
   parameter10 = "parameter10_field"
   parameter11 = "parameter11_field"
 }
+// ========================================================================== //
+// Multiple scripts
 
 resource "jamfpro_script" "multiple_script_min" {
   count = 100
-  name = "tf-testing-local-bw-min-${count.index}"
+  name = "tf-testing-min-${count.index}"
   script_contents = "echo hello world"
   priority = "BEFORE"
 }
 
 resource "jamfpro_script" "multiple_script_max" {
   count = 100
-  name = "tf-testing-local-bw-max-${count.index}"
-  script_contents = "echo hello world"
+  name = "tf-testing-max-${count.index}"
+  category_id = "9"
+  info = "info_field"
+  notes = "notes_field"
+  os_requirements = "os_requirements_field"
   priority = "BEFORE"
+  script_contents = "script_contents_field"
+  parameter4 = "parameter4_field"
+  parameter5 = "parameter5_field"
+  parameter6 = "parameter6_field"
+  parameter7 = "parameter7_field"
+  parameter8 = "parameter8_field"
+  parameter9 = "parameter9_field"
+  parameter10 = "parameter10_field"
+  parameter11 = "parameter11_field"
 }
