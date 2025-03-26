e9ab8875dc655ccfcff0fbc00c3e94c273641658
Bobby <bobby.james.williams@outlook.com>
2025-03-26 11:28:11 +0000

    add readme and clean up for testing filtering

--- testing/README.md
+++ testing/README.md
@@ -1,5 +1,6 @@
 # Provider Testing
 
+## Environment
 In each tesing module, there is a symlink to the testing root provider.tf. This is required for the tests to pass. 
 
 When adding new resource tests, please be sure that a symlink is correctly produced.
@@ -11,3 +12,22 @@ This can be achieved by inputting this command in the resource directory.
 
 
 The same result can be achieved by copying the file into the directories, without the benefit of only needing to alter one file for changes.
+
+To begin the tests, from `/testing` run 
+
+`terraform init`
+
+`terraform test`
+
+## Test case names
+
+To ensure only orphaned testing resources are removed in the clean up process, all testing resources must have a prefix of `tf-testing`. For example:
+
+```hcl
+resource "jamfpro_script" "min_script" {
+  name = "tf-testing-script-min"
+  script_contents = "script_contents_field"
+  priority = "BEFORE"
+}
+```
+
--- testing/jamfpy/clean_up.py
+++ testing/jamfpy/clean_up.py
@@ -15,16 +15,40 @@ instance = jamfpy.Tenant(
     token_exp_threshold_mins=1
 )
 
+def testing_ids_from_resources(resources):
+    resource_ids = []
+    for resource in resources:
+        name = resource["name"]
+        prefix = name[0:10]
+        if prefix == "tf-testing":
+            resource_id = resource["id"]
+            resource_ids.append(resource_id)
+    return resource_ids
+
+print("BEGIN PURGE")
+print("purging scripts")
 resp = instance.classic.scripts.get_all()
 resp.raise_for_status()
-all_scripts = resp.json()["scripts"]
-for i in all_scripts:
-    del_resp = instance.classic.scripts.delete_by_id(i["id"])
+resources = resp.json()["scripts"]
+resource_ids = testing_ids_from_resources(resources)
+for id in resource_ids:
+    del_resp = instance.classic.scripts.delete_by_id(id)
     print(del_resp.text)
 
+print("purging computer extension attributes")
 resp = instance.classic.computer_extension_attributes.get_all()
 resp.raise_for_status()
-all_scripts = resp.json()["computer_extension_attributes"]
-for i in all_scripts:
-    del_resp = instance.classic.computer_extension_attributes.delete_by_id(i["id"])
+computer_extension_attributes = resp.json()["computer_extension_attributes"]
+resource_ids = testing_ids_from_resources(computer_extension_attributes)
+for id in resource_ids:
+    del_resp = instance.classic.computer_extension_attributes.delete_by_id(id)
+    print(del_resp.text)
+
+print("purging buildings")
+resp = instance.classic.buildings.get_all()
+resp.raise_for_status()
+buildings = resp.json()["buildings"]
+resource_ids = testing_ids_from_resources(buildings)
+for id in resource_ids:
+    del_resp = instance.classic.buildings.delete_by_id(id)
     print(del_resp.text)
