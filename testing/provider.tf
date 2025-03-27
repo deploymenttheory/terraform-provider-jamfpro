c2b9eeea302096796bc9f664600f100f21f3f268
Bobby <bobby.james.williams@outlook.com>
2025-03-27 15:29:55 +0000

    rm comments and typos

--- testing/provider.tf
+++ testing/provider.tf
@@ -21,19 +21,6 @@ provider "jamfpro" {
   mandatory_request_delay_milliseconds = var.jamfpro_mandatory_request_delay_milliseconds
 }
 
-# provider "jamfpro" {
-#   jamfpro_instance_fqdn                = var.jamfpro_instance_fqdn
-#   auth_method                          = var.jamfpro_auth_method
-#   client_id                            = var.jamfpro_client_id
-#   client_secret                        = var.jamfpro_client_secret
-#   enable_client_sdk_logs               = true
-#   client_sdk_log_export_path           = "./log_dump/50/clientsdklogs/logs.txt"
-#   hide_sensitive_data                  = false
-#   jamfpro_load_balancer_lock           = var.jamfpro_load_balancer_lock
-#   token_refresh_buffer_period_seconds  = var.jamfpro_token_refresh_buffer_period_seconds
-#   mandatory_request_delay_milliseconds = var.jamfpro_mandatory_request_delay_milliseconds
-# }
-
 
 variable "jamfpro_instance_fqdn" {
   description = "The Jamf Pro FQDN (fully qualified domain name). Example: https://mycompany.jamfcloud.com"
