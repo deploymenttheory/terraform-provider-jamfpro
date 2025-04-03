dad12881c2dbfe9b3d7adfd87b68120b7d877a9e
Bobby <bobby.james.williams@outlook.com>
2025-04-03 14:52:34 +0100

    improve logging

--- testing/setup/cleanup.sh
+++ testing/setup/cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "BEGINNING CLEANUP"
-echo "Purging data source files in /data_sources..."
-rm ../data_sources/*
-echo "purging resources in jamfpro..."
+printf "\n##### BEGINNING CLEANUP #####\n"
+printf "Purging data source files in /data_sources...\n"
+rm -rf ../data_sources
+printf "purging resources in jamfpro...\n"
 python3 ../jamfpy/clean_up.py
--- testing/setup/scaffolding.sh
+++ testing/setup/scaffolding.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 bash ./cleanup.sh
-echo "Beginning scaffolding"
-echo "running computer_groups_scaffolding.py..."
+printf "\n##### Beginning scaffolding #####\n"
+printf "running computer_groups_scaffolding.py...\n"
 python3 ../jamfpy/static_computer_groups_scaffolding.py
