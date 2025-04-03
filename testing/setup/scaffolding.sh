326060350b25a17cf4bbb4de114a427b72f3917e
Bobby <bobby.james.williams@outlook.com>
2025-04-03 15:14:05 +0100

    separating concerns

--- testing/setup/scaffolding.sh
+++ testing/setup/scaffolding.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-bash ./cleanup.sh
 printf "\n##### Beginning scaffolding #####\n"
 printf "running computer_groups_scaffolding.py...\n"
 python3 ../jamfpy/static_computer_groups_scaffolding.py
