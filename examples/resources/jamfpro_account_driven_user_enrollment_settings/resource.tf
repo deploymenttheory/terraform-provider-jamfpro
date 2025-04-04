6482dd5c3e918b18e233c9000bd0a76fc8361ca3
ShocOne <62835948+ShocOne@users.noreply.github.com>
2025-04-04 09:43:07 +0100

    docs: fix up of example

--- examples/resources/jamfpro_account_driven_user_enrollment_settings/resource.tf
+++ examples/resources/jamfpro_account_driven_user_enrollment_settings/resource.tf
@@ -2,7 +2,7 @@ resource "jamfpro_account_driven_user_enrollment_settings" "example" {
   enabled                     = true
   expiration_interval_days    = 30
   # Optional - you can use days OR seconds, but not both
-  # expiration_interval_seconds = 2592000 # 30 days in seconds
+  expiration_interval_seconds = 2.592e+06 # 30 days in seconds. Must be in scienctific notation
 }
 
 # Output the configured settings
