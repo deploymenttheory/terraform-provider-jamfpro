183b3c9b06ba1f0bc294e667373f4221a50fb9bf
Neil Martin <neilmartin83@icloud.com>
2025-05-07 11:48:12 +0100

    refactor: remove unused token upload logic from device enrollment constructor

--- internal/resources/deviceenrollments/constructor.go
+++ internal/resources/deviceenrollments/constructor.go
@@ -18,21 +18,12 @@ func construct(d *schema.ResourceData) (*jamfpro.ResourceDeviceEnrollmentUpdate,
 		SiteId:                d.Get("site_id").(string),
 	}
 
-	tokenUpload := &jamfpro.ResourceDeviceEnrollmentTokenUpload{
-		TokenFileName: d.Get("token_file_name").(string),
-		EncodedToken:  d.Get("encoded_token").(string),
-	}
-
 	resourceJSON, err := json.MarshalIndent(resource, "", "  ")
 	if err != nil {
 		return nil, fmt.Errorf("failed to marshal Jamf Pro Device Enrollment '%s' to JSON: %v", resource.Name, err)
 	}
 
 	log.Printf("[DEBUG] Constructed Jamf Pro Device Enrollment JSON:\n%s\n", string(resourceJSON))
-
-	if tokenUpload.EncodedToken != "" {
-		d.Set("_token_upload", tokenUpload)
-	}
 
 	return resource, nil
 }
