947dfc487773ecac3700b8cf82d6ba34dd22a38d
Neil Martin <neilmartin83@icloud.com>
2025-05-08 22:09:56 +0100

    example and documentation update

--- examples/resources/jamfpro_sso_settings/resource.tf
+++ examples/resources/jamfpro_sso_settings/resource.tf
@@ -1,5 +1,5 @@
 resource "jamfpro_sso_settings" "google_example" {
-  sso_enabled                                          = false
+  sso_enabled                                          = true
   configuration_type                                   = "SAML"
   sso_bypass_allowed                                   = true
   sso_for_enrollment_enabled                           = true
@@ -26,6 +26,43 @@ resource "jamfpro_sso_settings" "google_example" {
     other_provider_type_name  = ""
     federation_metadata_file  = "PD94bWwgdmVyc2lvbj0iMS4wIiBlbm..."
     metadata_source           = "FILE"
+    session_timeout           = 480
+  }
+
+  enrollment_sso_config {
+    hosts           = []
+    management_hint = ""
+  }
+}
+
+resource "jamfpro_sso_settings" "entra_id_example" {
+  sso_enabled                                          = true
+  configuration_type                                   = "SAML"
+  sso_bypass_allowed                                   = true
+  sso_for_enrollment_enabled                           = true
+  sso_for_macos_self_service_enabled                   = true
+  enrollment_sso_for_account_driven_enrollment_enabled = false
+  group_enrollment_access_enabled                      = false
+  group_enrollment_access_name                         = ""
+
+  oidc_settings {
+    user_mapping = "EMAIL"
+  }
+
+  saml_settings {
+    token_expiration_disabled = true
+    user_attribute_enabled    = false
+    user_attribute_name       = ""
+    user_mapping              = "EMAIL"
+    group_attribute_name      = "http://schemas.microsoft.com/ws/2008/06/identity/claims/groups"
+    group_rdn_key             = ""
+    idp_provider_type         = "AZURE"
+    idp_url                   = "https://example.idp.com/app/id/sso/saml/metadata"
+    entity_id                 = "saml/metadata"
+    metadata_file_name        = ""
+    other_provider_type_name  = ""
+    federation_metadata_file  = ""
+    metadata_source           = "URL"
     session_timeout           = 480
   }
 
