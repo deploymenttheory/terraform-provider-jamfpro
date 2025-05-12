ef3bbc0f454910408860c2819bf6e7f2a95a17ca
Neil Martin <neilmartin83@icloud.com>
2025-05-12 13:40:12 +0100

    use standard crud functions for ID based resources

--- internal/resources/volumepurchasinglocations/crud.go
+++ internal/resources/volumepurchasinglocations/crud.go
@@ -2,42 +2,24 @@ package volumepurchasinglocations
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/deploymenttheory/go-api-sdk-jamfpro/sdk/jamfpro"
 	"github.com/deploymenttheory/terraform-provider-jamfpro/internal/resources/common"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 // create is responsible for creating a new volume purchasing location in the remote system.
 func create(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	client := meta.(*jamfpro.Client)
-	var diags diag.Diagnostics
 
-	resource, err := construct(d)
-	if err != nil {
-		return diag.FromErr(fmt.Errorf("failed to construct Jamf Pro Volume Purchasing Location: %v", err))
-	}
-
-	var creationResponse *jamfpro.ResponseVolumePurchasingLocationCreate
-	err = retry.RetryContext(ctx, d.Timeout(schema.TimeoutCreate), func() *retry.RetryError {
-		var apiErr error
-		creationResponse, apiErr = client.CreateVolumePurchasingLocation(resource)
-		if apiErr != nil {
-			return retry.RetryableError(apiErr)
-		}
-		return nil
-	})
-
-	if err != nil {
-		return diag.FromErr(fmt.Errorf("failed to create Jamf Pro Volume Purchasing Location '%s' after retries: %v", resource.Name, err))
-	}
-
-	d.SetId(creationResponse.ID)
-
-	return append(diags, readNoCleanup(ctx, d, meta)...)
+	return common.Create(
+		ctx,
+		d,
+		meta,
+		construct,
+		meta.(*jamfpro.Client).CreateVolumePurchasingLocation,
+		readNoCleanup,
+	)
 }
 
 // read is responsible for reading the current state of a Volume Purchasing Location from the remote system.
@@ -62,33 +44,19 @@ func readNoCleanup(ctx context.Context, d *schema.ResourceData, meta interface{}
 	return read(ctx, d, meta, false)
 }
 
-// update is responsible for updating an existing Volume Purchasing Location on the remote system.
+// update is responsible for updating an existing volume purchasing location on the remote system.
 func update(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	client := meta.(*jamfpro.Client)
-	var diags diag.Diagnostics
-	resourceID := d.Id()
-
-	resource, err := construct(d)
-	if err != nil {
-		return diag.FromErr(fmt.Errorf("failed to construct Jamf Pro Volume Purchasing Location for update: %v", err))
-	}
-
-	err = retry.RetryContext(ctx, d.Timeout(schema.TimeoutUpdate), func() *retry.RetryError {
-		_, apiErr := client.UpdateVolumePurchasingLocationByID(resourceID, resource)
-		if apiErr != nil {
-			return retry.RetryableError(apiErr)
-		}
-		return nil
-	})
-
-	if err != nil {
-		return diag.FromErr(fmt.Errorf("failed to update Jamf Pro Volume Purchasing Location '%s' (ID: %s) after retries: %v", resource.Name, resourceID, err))
-	}
-
-	return append(diags, readNoCleanup(ctx, d, meta)...)
+	return common.Update(
+		ctx,
+		d,
+		meta,
+		construct,
+		meta.(*jamfpro.Client).UpdateVolumePurchasingLocationByID,
+		readNoCleanup,
+	)
 }
 
-// delete is responsible for deleting a Jamf Pro Volume Purchasing Location.
+// delete is responsible for deleting a volume purchasing location.
 func delete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	return common.Delete(
 		ctx,
