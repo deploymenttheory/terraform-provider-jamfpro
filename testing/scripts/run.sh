0ba53ec2fedd32ac2d945f53dfdbd44d59826be1
Joseph <joelittle11@live.com>
2025-05-27 14:18:01 +0100

    more arg

--- testing/scripts/run.sh
+++ testing/scripts/run.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 TARGETS="$1"
+UUID="$2"
 
 if [[ "$TARGETS" == *"jamfpro_static_computer_group"* ]]; then
-    ./scaffolding.sh
+    ./scaffolding.sh -r "$UUID"
 fi
 
+terraform init
 terraform fmt
 terraform validate
 terraform test
