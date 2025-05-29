ad8c16ce99b2f756a2d32ab07f1f8d3c85994590
Joseph <joelittle11@live.com>
2025-05-29 15:34:36 +0100

    execution perms

--- testing/scripts/start.sh
+++ testing/scripts/start.sh
@@ -5,6 +5,7 @@ UUID="$2"
 
 if [[ "$TARGETS" == *"jamfpro_static_computer_group"* ]]; then
     echo running scaffolding
+    chmod +x ./scripts/static_computer_groups_scaffolding.py
     ./scripts/static_computer_groups_scaffolding.py -r "$UUID"
 fi
 
