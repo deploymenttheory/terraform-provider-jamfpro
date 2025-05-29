c3a51e2ce2e1be3498c4383cf582d148207f5b15
Joseph <joelittle11@live.com>
2025-05-29 15:29:29 +0100

    oops

--- testing/scripts/start.sh
+++ testing/scripts/start.sh
@@ -5,7 +5,7 @@ UUID="$2"
 
 if [[ "$TARGETS" == *"jamfpro_static_computer_group"* ]]; then
     echo running scaffolding
-    ./scripts/computer_groups_scaffolding.py -r "$UUID"
+    ./scripts/static_computer_groups_scaffolding.py -r "$UUID"
 fi
 
 terraform init
