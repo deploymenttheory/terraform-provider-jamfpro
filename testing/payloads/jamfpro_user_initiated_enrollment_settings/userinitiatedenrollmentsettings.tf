3764477543be505be2a118f00adb1b2e15b1610d
Neil Martin <neilmartin83@icloud.com>
2025-06-22 20:28:33 +0100

    fix: correct state management of user_initiated_enrollment_settings

--- testing/payloads/jamfpro_user_initiated_enrollment_settings/userinitiatedenrollmentsettings.tf
+++ testing/payloads/jamfpro_user_initiated_enrollment_settings/userinitiatedenrollmentsettings.tf
@@ -1,34 +1,56 @@
-resource "jamfpro_user_initiated_enrollment_settings" "user_initiated_enrollment_settings" {
-  restrict_reenrollment_to_authorized_users_only  = false
+# Configure Jamf Pro User Initiated Enrollment Settings
+resource "jamfpro_user_initiated_enrollment_settings" "jamfpro_uie_settings" {
+  # General Settings
+  restrict_reenrollment_to_authorized_users_only  = true
   skip_certificate_installation_during_enrollment = true
 
+  # Flush Settings
   flush_location_information         = true
   flush_location_history_information = true
   flush_policy_history               = true
   flush_extension_attributes         = true
-  flush_software_update_plans        = true
+  flush_software_update_plans        = false
   flush_mdm_commands_on_reenroll     = "DELETE_EVERYTHING"
 
+  # Computer Enrollment Settings
   user_initiated_enrollment_for_computers {
     enable_user_initiated_enrollment_for_computers = true
     ensure_ssh_is_enabled                          = false
-    launch_self_service_when_done                  = true
+    launch_self_service_when_done                  = false
     account_driven_device_enrollment               = false
 
+    # Managed Local Administrator Account
     managed_local_administrator_account {
       create_managed_local_administrator_account                    = true
-      management_account_username                                   = "lapsadmin"
+      management_account_username                                   = "jamfadmin"
       hide_managed_local_administrator_account                      = true
       allow_ssh_access_for_managed_local_administrator_account_only = false
     }
   }
 
+  # Mobile Device Enrollment Settings
   user_initiated_enrollment_for_devices {
+    # Profile-Driven Enrollment
     profile_driven_enrollment_via_url {
-      enable_for_institutionally_owned_devices = true
-      enable_for_personally_owned_devices      = false
+      enable_for_institutionally_owned_devices = false
+      enable_for_personally_owned_devices      = true
+      personal_device_enrollment_type          = "USERENROLLMENT"
+    }
+
+    # Account-Driven User Enrollment
+    account_driven_user_enrollment {
+      enable_for_personally_owned_mobile_devices     = true
+      enable_for_personally_owned_vision_pro_devices = false
+    }
+
+    # Account-Driven Device Enrollment
+    account_driven_device_enrollment {
+      enable_for_institutionally_owned_mobile_devices = false
+      enable_for_personally_owned_vision_pro_devices  = true
     }
   }
+
+  # Enrollment Messaging - English (this block is always required. It's built into the jamf gui)
   messaging {
     language_code                                   = "en"
     language_name                                   = "english"
@@ -75,15 +97,50 @@ resource "jamfpro_user_initiated_enrollment_settings" "user_initiated_enrollment
     log_out_button_name                             = "Log Out"
   }
 
-  directory_service_group_enrollment_settings {
-    id                                                                       = "1"
-    allow_group_to_enroll_institutionally_owned_devices                      = true
-    allow_group_to_enroll_personally_owned_devices                           = true
-    allow_group_to_enroll_personal_and_institutionally_owned_devices_via_ade = true
-    require_eula                                                             = true
-    ldap_server_id                                                           = "-1"
-    directory_service_group_name                                             = "All Directory Service Users"
-    directory_service_group_id                                               = "-1"
-    site_id                                                                  = "-1"
+  # Enrollment Messaging - French (All additional languages are optional)
+  messaging {
+    language_code                                   = "fr"
+    language_name                                   = "french"
+    page_title                                      = "Welcome to Device Enrollment"
+    username_text                                   = "Username"
+    password_text                                   = "Password"
+    login_button_text                               = "Log In"
+    device_ownership_page_text                      = "Select device type"
+    personal_device_button_name                     = "Personal Device"
+    institutional_ownership_button_name             = "Company Device"
+    personal_device_management_description          = "Personal device with minimal management"
+    institutional_device_management_description     = "Company device with full management"
+    enroll_device_button_name                       = "Enroll Device"
+    eula_personal_devices                           = "Agreement for personal device management"
+    eula_institutional_devices                      = "Agreement for company device management"
+    accept_button_text                              = "Accept"
+    site_selection_text                             = "Select site"
+    ca_certificate_installation_text                = "Install Certificate"
+    ca_certificate_name                             = "Root Certificate"
+    ca_certificate_description                      = "Security certificate"
+    ca_certificate_install_button_name              = "Install"
+    institutional_mdm_profile_installation_text     = "Install Profile"
+    institutional_mdm_profile_name                  = "MDM Profile"
+    institutional_mdm_profile_description           = "Management profile"
+    institutional_mdm_profile_pending_text          = "Installing..."
+    institutional_mdm_profile_install_button_name   = "Install"
+    personal_mdm_profile_installation_text          = "Install Profile"
+    personal_mdm_profile_name                       = "Personal Profile"
+    personal_mdm_profile_description                = "Management profile"
+    personal_mdm_profile_install_button_name        = "Install"
+    user_enrollment_mdm_profile_installation_text   = "Install Profile"
+    user_enrollment_mdm_profile_name                = "User Profile"
+    user_enrollment_mdm_profile_description         = "User enrollment profile"
+    user_enrollment_mdm_profile_install_button_name = "Install"
+    quickadd_package_installation_text              = "Install Software"
+    quickadd_package_name                           = "Management Agent"
+    quickadd_package_progress_text                  = "Installing..."
+    quickadd_package_install_button_name            = "Install"
+    enrollment_complete_text                        = "Enrollment Complete"
+    enrollment_failed_text                          = "Enrollment Failed"
+    try_again_button_name                           = "Try Again"
+    view_enrollment_status_button_name              = "Check Status"
+    view_enrollment_status_text                     = "View enrollment status"
+    log_out_button_name                             = "Log Out"
   }
 }
