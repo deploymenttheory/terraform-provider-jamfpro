62ee78cab5be094e39529be879eef29bed9592cb
Emmanuel Canault <ecanault@netopie.fr>
2025-06-23 16:23:04 +0200

    fix tests

--- testing/payloads/jamfpro_jamf_connect/jamf_connect_configuration_profiles.tf
+++ testing/payloads/jamfpro_jamf_connect/jamf_connect_configuration_profiles.tf
@@ -3,16 +3,38 @@
 // ========================================================================== //
 
 // ========================================================================== //
+// Create configuration profile for testing
+
+resource "jamfpro_macos_configuration_profile_plist" "jamf_connect_license_001" {
+  name                = "Jamf Connect License"
+  level               = "System"
+  distribution_method = "Install Automatically"
+  redeploy_on_update  = "Newly Assigned"
+  payloads            = file("./jamf_connect_license.mobileconfig")
+  payload_validate    = true
+  user_removable      = false
+  scope {
+    all_computers = true
+    all_jss_users = false
+  }
+}
+
+// ========================================================================== //
 // Data source by configuration profile ID
 
 data "jamfpro_jamf_connect" "by_id" {
-  profile_id = 429
+  depends_on = [
+    jamfpro_macos_configuration_profile_plist.jamf_connect_license_001
+  ]
+  profile_id = jamfpro_macos_configuration_profile_plist.jamf_connect_license_001.id
 }
 
 // ========================================================================== //
 // Data source by configuration profile name
 
 data "jamfpro_jamf_connect" "by_name" {
-  profile_name = "cp_jamfconnect_license"
+  depends_on = [
+    jamfpro_macos_configuration_profile_plist.jamf_connect_license_001
+  ]
+  profile_name = "Jamf Connect License"
 }
-
