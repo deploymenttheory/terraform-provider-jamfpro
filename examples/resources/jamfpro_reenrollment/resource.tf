bdbba59bc448ad8904bd1fedb74b64fb13a051f9
Emmanuel Canault <ecanault@netopie.fr>
2025-07-03 17:38:57 +0200

    typos

--- examples/resources/jamfpro_reenrollment/resource.tf
+++ examples/resources/jamfpro_reenrollment/resource.tf
@@ -4,5 +4,5 @@ resource "jamfpro_reenrollment" "settings" {
   flush_policy_history               = true
   flush_extension_attributes         = true
   flush_software_update_plans        = true
-  flush_mdm_queue                    = "DELETE_NOTHING" // required, valide values: DELETE_NOTHING, DELETE_ERRORS, DELETE_EVERYTHING_EXCEPT_ACKNOWLEDGED, DELETE_EVERYTHING
+  flush_mdm_queue                    = "DELETE_EVERYTHING" // required, valid values: DELETE_NOTHING, DELETE_ERRORS, DELETE_EVERYTHING_EXCEPT_ACKNOWLEDGED, DELETE_EVERYTHING
 }
--- testing/payloads/jamfpro_reenrollment/reenrollment.tf
+++ testing/payloads/jamfpro_reenrollment/reenrollment.tf
@@ -11,5 +11,5 @@ resource "jamfpro_reenrollment" "settings" {
   flush_policy_history               = true
   flush_extension_attributes         = true
   flush_software_update_plans        = true
-  flush_mdm_queue                    = "DELETE_NOTHING"
+  flush_mdm_queue                    = "DELETE_EVERYTHING"
 }
