2dd3dbb87953174a1c218233fee3f748d8280dc4
Neil Martin <neil.martin@jamf.com>
2025-08-31 13:45:09 +0100

    chore: add test payloads

---
+++ testing/payloads/jamfpro_macos_configuration_profile_plist/main.tf
@@ -0,0 +1,16 @@
+resource "jamfpro_macos_configuration_profile_plist" "jamfpro_macos_configuration_profile_064" {
+  name                = "test-profile"
+  description         = "An example mobile device configuration profile."
+  level               = "System"
+  distribution_method = "Install Automatically"
+  redeploy_on_update  = "Newly Assigned"
+  payloads            = file("${path.module}/Screen Recording - Allow Microsoft Teams.mobileconfig")
+  payload_validate    = false
+  user_removable      = false
+
+  scope {
+    all_computers = true
+    all_jss_users = false
+  }
+
+}
