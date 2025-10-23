042f3da75038e6359bcf1a1f4da407b6201931e0
ShocOne <62835948+ShocOne@users.noreply.github.com>
2025-10-23 10:46:05 +0100

    chore: added missing file name changes

--- internal/services/static_computer_group/data_source_read.go
+++ internal/services/static_computer_group/data_source_read.go
@@ -1,4 +1,3 @@
-// staticcomputergroup_data_source.go
 package static_computer_group
 
 import (
@@ -10,44 +9,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
-
-func DataSourceJamfProStaticComputerGroups() *schema.Resource {
-	return &schema.Resource{
-		ReadContext: dataSourceRead,
-		Schema: map[string]*schema.Schema{
-			"id": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Computed:    true,
-				Description: "The unique identifier of the static computer group.",
-			},
-			"name": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Computed:    true,
-				Description: "The name of the static computer group.",
-			},
-			"is_smart": {
-				Type:        schema.TypeBool,
-				Computed:    true,
-				Description: "Whether this is a smart group.",
-			},
-			"site_id": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "The site ID for the group.",
-			},
-			"assigned_computer_ids": {
-				Type:     schema.TypeList,
-				Computed: true,
-				Elem: &schema.Schema{
-					Type: schema.TypeInt,
-				},
-				Description: "List of assigned computer IDs.",
-			},
-		},
-	}
-}
 
 func dataSourceRead(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
 	client := meta.(*jamfpro.Client)
