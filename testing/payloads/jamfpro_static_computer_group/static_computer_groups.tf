ecbeba8601bb98b5c52c314fe5357f01701deaaf
Neil Martin <neil.martin@jamf.com>
2025-11-24 08:34:17 +0000

    feat: add local file data source for site and computer IDs in static computer group

--- testing/payloads/jamfpro_static_computer_group/static_computer_groups.tf
+++ testing/payloads/jamfpro_static_computer_group/static_computer_groups.tf
@@ -2,7 +2,13 @@
 // Static Computer Groups
 // ========================================================================== //
 
+data "local_file" "site_and_computer_ids" {
+  filename = "testing/data_sources/site_and_computer_ids.json"
+}
+
 resource "jamfpro_static_computer_group" "name" {
-  name        = "tf-testing-${var.testing_id}-script-max-${random_id.rng.hex}"
-  description = "Terraform Static Computer Group for testing purposes."
+  name                  = "tf-testing-${var.testing_id}-script-max-${random_id.rng.hex}"
+  description           = "Terraform Static Computer Group for testing purposes."
+  site_id               = jsondecode(data.local_file.site_and_computer_ids.content).site
+  assigned_computer_ids = jsondecode(data.local_file.site_and_computer_ids.content).computers
 }
