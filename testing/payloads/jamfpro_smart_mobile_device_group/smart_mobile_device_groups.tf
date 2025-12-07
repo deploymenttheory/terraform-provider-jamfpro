3ac0e629d6693281af670067876333047e0d4ed0
Neil Martin <neil.martin@jamf.com>
2025-12-07 17:25:14 +0000

    fix: remove unused data source for site and computer IDs in smart groups

--- testing/payloads/jamfpro_smart_mobile_device_group/smart_mobile_device_groups.tf
+++ testing/payloads/jamfpro_smart_mobile_device_group/smart_mobile_device_groups.tf
@@ -2,14 +2,9 @@
 // Smart Computer Groups
 // ========================================================================== //
 
-data "local_file" "site_and_computer_ids" {
-  filename = "testing/data_sources/site_and_computer_ids.json"
-}
-
 resource "jamfpro_smart_mobile_device_group" "name" {
   name        = "tf-testing-${var.testing_id}-script-max-${random_id.rng.hex}"
   description = "Terraform testing smart mobile device group."
-  site_id     = jsondecode(data.local_file.site_and_computer_ids.content).site
   criteria {
     name        = "Serial Number"
     search_type = "not like"
