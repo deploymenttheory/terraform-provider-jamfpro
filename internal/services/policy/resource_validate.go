bb5ea650abcbd23ef8d19803fb0b3b931b8c0ae1
Tyler Sparr <tyler.sparr@encore.tech>
2025-12-19 14:16:51 -0500

    Add validation to require use_for_self_service when self_service block is configured

--- internal/services/policy/resource_validate.go
+++ internal/services/policy/resource_validate.go
@@ -47,47 +47,17 @@ func validate12HourTime(v any, k string) (warns []string, errs []error) {
 }
 
 // validateSelfServiceConfig validates that when use_for_self_service is false,
-// no other self service attributes should be configured with non-default values
+// the self_service block should not be configured
 func validateSelfServiceConfig(ctx context.Context, d *schema.ResourceDiff, meta interface{}) error {
 	if selfService, ok := d.GetOk("self_service"); ok {
 		selfServiceList := selfService.([]interface{})
 		if len(selfServiceList) > 0 {
 			selfServiceMap := selfServiceList[0].(map[string]interface{})
-			useForSelfService := selfServiceMap["use_for_self_service"].(bool)
+			useForSelfService, exists := selfServiceMap["use_for_self_service"]
 
-			// If self service is disabled, check that no other attributes are set to non-default values
-			if !useForSelfService {
-				defaults := map[string]interface{}{
-					"self_service_display_name":       "",
-					"install_button_text":             "Install",
-					"reinstall_button_text":           "Reinstall",
-					"self_service_description":        "",
-					"force_users_to_view_description": false,
-					"self_service_icon_id":            0,
-					"feature_on_main_page":            false,
-					"notification":                    false,
-					"notification_type":               "Self Service",
-					"notification_subject":            "",
-					"notification_message":            "",
-				}
-
-				var nonDefaultAttrs []string
-				for key, defaultValue := range defaults {
-					if actualValue, exists := selfServiceMap[key]; exists && actualValue != defaultValue {
-						nonDefaultAttrs = append(nonDefaultAttrs, key)
-					}
-				}
-
-				// Check for self_service_category
-				if categories, exists := selfServiceMap["self_service_category"]; exists {
-					if categoriesList, ok := categories.([]interface{}); ok && len(categoriesList) > 0 {
-						nonDefaultAttrs = append(nonDefaultAttrs, "self_service_category")
-					}
-				}
-
-				if len(nonDefaultAttrs) > 0 {
-					return fmt.Errorf("when use_for_self_service is false, the following self_service attributes should not be configured: %v. Either set use_for_self_service to true or remove these attributes", nonDefaultAttrs)
-				}
+			// If use_for_self_service is not explicitly set to true, error out
+			if !exists || !useForSelfService.(bool) {
+				return fmt.Errorf("self_service block is configured but use_for_self_service is not set to true. Either set use_for_self_service = true or remove the self_service block entirely")
 			}
 		}
 	}
