d73571cd696c49e25da7cad1353bd84aa5ac40f1
Neil Martin <neil.martin@jamf.com>
2026-01-18 15:15:50 +0000

    feat: implement custom diff validation for mobile device extension attributes

--- testing/payloads/jamfpro_mobile_device_extension_attribute/mobile_device_extension_attribute.tf
+++ testing/payloads/jamfpro_mobile_device_extension_attribute/mobile_device_extension_attribute.tf
@@ -1,23 +1,56 @@
-# Pop-up menu Example
-resource "jamfpro_mobile_device_extension_attribute" "popup_menu_example" {
-  name                   = "Device Location Test"
-  description            = "The primary location where this device is used"
-  data_type              = "STRING"
-  inventory_display_type = "USER_AND_LOCATION"
-  input_type             = "POPUP"
-  popup_menu_choices = [
-    "Head Office",
-    "Branch Office",
-    "Home Office",
-    "Client Site"
-  ]
-}
+// ========================================================================== //
+// Mobile Device Extension Attributes
+// ========================================================================== //
 
-# Text Field Example
-resource "jamfpro_mobile_device_extension_attribute" "text_field_example" {
-  name                   = "User Department Test"
-  description            = "The department to which the device user belongs"
+// ========================================================================== //
+// UNTESTED RESOURCES
+# - input_type  = "DIRECTORY_SERVICE_ATTRIBUTE_MAPPING"
+
+// ========================================================================== //
+// Single extension attributes
+
+resource "jamfpro_mobile_device_extension_attribute" "jamfpro_mobile_device_extension_attribute_max_text" {
+  name                   = "tf-testing-${var.testing_id}-max-text-${random_id.rng.hex}"
+  description            = "description"
   data_type              = "STRING"
   inventory_display_type = "GENERAL"
   input_type             = "TEXT"
+}
+resource "jamfpro_mobile_device_extension_attribute" "jamfpro_mobile_device_extension_attribute_max_popup" {
+  name                   = "tf-testing-${var.testing_id}-max-popup-${random_id.rng.hex}"
+  description            = "description"
+  data_type              = "STRING"
+  enabled                = true
+  inventory_display_type = "GENERAL"
+  input_type             = "POPUP"
+  popup_menu_choices     = ["Option 1", "Option 2", "Option 3"]
+}
+
+// Test that minimally defined object is sufficient
+resource "jamfpro_mobile_device_extension_attribute" "jamfpro_mobile_device_extension_attribute_minimum" {
+  name                   = "tf-testing-${var.testing_id}-min-test"
+  input_type             = "TEXT"
+  data_type              = "STRING"
+  inventory_display_type = "GENERAL"
+}
+
+// ========================================================================== //
+// Multiple extension attributes
+
+resource "jamfpro_mobile_device_extension_attribute" "jamfpro_mobile_device_extension_attribute_max_text_multiple" {
+  count                  = 100
+  name                   = "tf-testing-${var.testing_id}-max-text-${count.index}-${random_id.rng.hex}"
+  description            = "description"
+  data_type              = "STRING"
+  inventory_display_type = "GENERAL"
+  input_type             = "TEXT"
+}
+resource "jamfpro_mobile_device_extension_attribute" "jamfpro_mobile_device_extension_attribute_max_popup_multiple" {
+  count                  = 100
+  name                   = "tf-testing-${var.testing_id}-max-popup-${count.index}-${random_id.rng.hex}"
+  description            = "description"
+  data_type              = "STRING"
+  inventory_display_type = "GENERAL"
+  input_type             = "POPUP"
+  popup_menu_choices     = ["Option 1", "Option 2", "Option 3"]
 }
