b2e9986874f0e31cae6b0ebdedbdad344a3d411f
Neil Martin <neil.martin@jamf.com>
2026-01-27 12:16:17 +0000

    feat: add jamfpro_adcs_settings resource

--- internal/provider/framework_resources.go
+++ internal/provider/framework_resources.go
@@ -3,12 +3,14 @@ package provider
 import (
 	"context"
 
+	jamfProAdcsSettings "github.com/deploymenttheory/terraform-provider-jamfpro/internal/services/adcs_settings"
 	jamfProDockItem "github.com/deploymenttheory/terraform-provider-jamfpro/internal/services/dock_item"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 )
 
 func (p *frameworkProvider) Resources(ctx context.Context) []func() resource.Resource {
 	return []func() resource.Resource{
+		jamfProAdcsSettings.NewAdcsSettingsFrameworkResource,
 		jamfProDockItem.NewDockItemFrameworkResource,
 	}
 }
