1d20e7dc2021de5b575457ceac8cdc73adbede6d
Neil Martin <neil.martin@jamf.com>
2026-02-03 17:22:14 +0000

    fix: update condition to include jamfpro_static_mobile_device_group in scaffolding script

--- testing/action_scripts/start_test.sh
+++ testing/action_scripts/start_test.sh
@@ -3,7 +3,7 @@
 TARGETS="$1"
 UUID="$2"
 
-if [[ "$TARGETS" == *"jamfpro_static_computer_group"* ]] || [[ "$TARGETS" == "all" ]]; then
+if [[ "$TARGETS" == *"jamfpro_static_computer_group"* ]] || [[ "$TARGETS" == *"jamfpro_static_mobile_device_group"* ]] || [[ "$TARGETS" == "all" ]]; then
     echo running scaffolding
     python3 ./action_scripts/scaffolding_static_group_computers.py -r "$UUID"
     python3 ./action_scripts/scaffolding_static_group_mobile_devices.py -r "$UUID"
